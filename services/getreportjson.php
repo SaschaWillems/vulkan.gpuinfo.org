b5a8b85e7a3dc1f0014146f029f2015ac69666e0
saschawillems <webmaster@saschawillems.de>
2016-05-19 21:15:56 +0200

    Added service to retrieve a report as json

---
+++ services/getreportjson.php
@@ -0,0 +1,100 @@
+<?php
+	/* 		
+	*
+	* Vulkan hardware capability database server implementation
+	*	
+	* Copyright (C) 2011-2015 by Sascha Willems (www.saschawillems.de)
+	*	
+	* This code is free software, you can redistribute it and/or
+	* modify it under the terms of the GNU Affero General Public
+	* License version 3 as published by the Free Software Foundation.
+	*	
+	* Please review the following information to ensure the GNU Lesser
+	* General Public License version 3 requirements will be met:
+	* http://www.gnu.org/licenses/agpl-3.0.de.html
+	*	
+	* The code is distributed WITHOUT ANY WARRANTY; without even the
+	* implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+	* PURPOSE.  See the GNU AGPL 3.0 for more details.		
+	*
+	*/
+
+	// Return report as json (uploaded from client application)
+	
+	include './../dbconfig.php';
+	
+	/**
+	 * Formats a JSON string for pretty printing
+	 *
+	 * @param string $json The JSON to make pretty
+	 * @param bool $html Insert nonbreaking spaces and <br />s for tabs and linebreaks
+	 * @return string The prettified output
+	 * @author Jay Roberts (https://github.com/GloryFish)
+	 */
+     function _format_json($json, $html = false) {
+		$tabcount = 0; 
+		$result = ''; 
+		$inquote = false; 
+		$ignorenext = false; 
+		if ($html) { 
+		    $tab = "&nbsp;&nbsp;&nbsp;"; 
+		    $newline = "<br/>"; 
+		} else { 
+		    $tab = "\t"; 
+		    $newline = "\n"; 
+		} 
+		for($i = 0; $i < strlen($json); $i++) { 
+		    $char = $json[$i]; 
+		    if ($ignorenext) { 
+		        $result .= $char; 
+		        $ignorenext = false; 
+		    } else { 
+		        switch($char) { 
+		            case '{': 
+		                $tabcount++; 
+		                $result .= $char . $newline . str_repeat($tab, $tabcount); 
+		                break; 
+		            case '}': 
+		                $tabcount--; 
+		                $result = trim($result) . $newline . str_repeat($tab, $tabcount) . $char; 
+		                break; 
+		            case ',': 
+		                $result .= $char . $newline . str_repeat($tab, $tabcount); 
+		                break; 
+		            case '"': 
+		                $inquote = !$inquote; 
+		                $result .= $char; 
+		                break; 
+		            case '\\': 
+		                if ($inquote) $ignorenext = true; 
+		                $result .= $char; 
+		                break; 
+		            default: 
+		                $result .= $char; 
+		        } 
+		    } 
+		} 
+		return $result; 
+	}
+	
+	dbConnect();	
+			
+	$reportid = mysql_real_escape_string($_GET['id']);	
+	
+	$sqlresult = mysql_query("select reportid, json from reportsjson where reportid = ".$reportid) or die(mysql_error());
+	$sqlcount = mysql_num_rows($sqlresult);   
+	$sqlrow = mysql_fetch_row($sqlresult);
+	
+	if ($sqlcount > 0) 
+	{
+		header('HTTP/ 200 report_present '.$sqlrow[0].'');
+		echo _format_json($sqlrow[1], true);
+	} 
+	else 
+	{
+		header('HTTP/ 404 report_not_present');
+		echo "report not present";
+	}
+
+	dbDisconnect();	
+?>
