01746175d76df9af486dfeee4f9fd2ad44cf301b
saschawillems <webmaster@saschawillems.de>
2016-11-18 18:30:26 +0100

    Check for valid surface before inserting surface capabilities

--- services/uploadreport.php
+++ services/uploadreport.php
@@ -288,54 +288,58 @@
 	$hassurfacecaps = false;
 	if (array_key_exists('surfacecapabilites', $json))
 	{
-		$hassurfacecaps = true;
-		// Caps
 		$surfacecaps = $json['surfacecapabilites'];
-		$sqlstr = "
-			INSERT INTO devicesurfacecapabilities
-				(reportid, minImageCount, maxImageCount, maxImageArrayLayers, `minImageExtent.width`, `minImageExtent.height`, 
-				`maxImageExtent.width`, `maxImageExtent.height`, supportedUsageFlags, supportedTransforms, supportedCompositeAlpha)
-			VALUES
-				(".
-					$reportid.",".
-					$surfacecaps['minImageCount'].",".
-					$surfacecaps['maxImageCount'].",".
-					$surfacecaps['maxImageArrayLayers'].",".
-					$surfacecaps['minImageExtent.width'].",".
-					$surfacecaps['minImageExtent.height'].",".
-					$surfacecaps['maxImageExtent.width'].",".
-					$surfacecaps['maxImageExtent.height'].",".
-					$surfacecaps['supportedUsageFlags'].",".
-					$surfacecaps['supportedTransforms'].",".
-					$surfacecaps['supportedCompositeAlpha'].
-				")";							
-		$sqlresult = mysql_query($sqlstr) or die(mysql_error()."\n".$sqlstr);
-		// Present modes
-		$jsonnode = $json['surfacecapabilites']['presentmodes']; 
-		if (is_array($jsonnode)) {
-			foreach ($jsonnode as $presentmode)
-			{
-				$sqlstr = "
-					insert into devicesurfacemodes
-						(reportid, presentmode)
-					values	
-						($reportid, ".$presentmode['presentMode'].")";
-				$sqlresult = mysql_query($sqlstr) or die(mysql_error());		
-			}
-		}	
-		// Surface formats	 		
-		$jsonnode = $json['surfacecapabilites']['surfaceformats']; 
-		if (is_array($jsonnode)) {
-			foreach ($jsonnode as $surfaceformat)
-			{
-				$sqlstr = "
-					insert into devicesurfaceformats
-						(reportid, format, colorSpace)
-					values	
-						($reportid, ".$surfaceformat['format'].", ".$surfaceformat['colorSpace'].")";
-				$sqlresult = mysql_query($sqlstr) or die(mysql_error());		
-			}
-		}	
+		$hassurfacecaps = ($surfacecaps['validSurface'] == 1);
+		if ($hassurfacecaps)
+		{
+			// Caps
+			$sqlstr = "
+				INSERT INTO devicesurfacecapabilities
+					(reportid, minImageCount, maxImageCount, maxImageArrayLayers, `minImageExtent.width`, `minImageExtent.height`, 
+					`maxImageExtent.width`, `maxImageExtent.height`, supportedUsageFlags, supportedTransforms, supportedCompositeAlpha, surfaceExtension)
+				VALUES
+					(".
+						$reportid.",".
+						$surfacecaps['minImageCount'].",".
+						$surfacecaps['maxImageCount'].",".
+						$surfacecaps['maxImageArrayLayers'].",".
+						$surfacecaps['minImageExtent.width'].",".
+						$surfacecaps['minImageExtent.height'].",".
+						$surfacecaps['maxImageExtent.width'].",".
+						$surfacecaps['maxImageExtent.height'].",".
+						$surfacecaps['supportedUsageFlags'].",".
+						$surfacecaps['supportedTransforms'].",".
+						$surfacecaps['supportedCompositeAlpha'].",".
+						"'".$surfacecaps['surfaceExtension']."'".
+					")";							
+			$sqlresult = mysql_query($sqlstr) or die(mysql_error()."\n".$sqlstr);
+			// Present modes
+			$jsonnode = $json['surfacecapabilites']['presentmodes']; 
+			if (is_array($jsonnode)) {
+				foreach ($jsonnode as $presentmode)
+				{
+					$sqlstr = "
+						insert into devicesurfacemodes
+							(reportid, presentmode)
+						values	
+							($reportid, ".$presentmode['presentMode'].")";
+					$sqlresult = mysql_query($sqlstr) or die(mysql_error());		
+				}
+			}	
+			// Surface formats	 		
+			$jsonnode = $json['surfacecapabilites']['surfaceformats']; 
+			if (is_array($jsonnode)) {
+				foreach ($jsonnode as $surfaceformat)
+				{
+					$sqlstr = "
+						insert into devicesurfaceformats
+							(reportid, format, colorSpace)
+						values	
+							($reportid, ".$surfaceformat['format'].", ".$surfaceformat['colorSpace'].")";
+					$sqlresult = mysql_query($sqlstr) or die(mysql_error());		
+				}
+			}	
+		}
 	}
 		
 	mysql_query("COMMIT");
