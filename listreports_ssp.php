c0a6ca1952db440eb40e2de07f11509689c6953d
saschawillems <webmaster@saschawillems.de>
2017-04-30 12:05:20 +0200

    Server-side processing of report list (with combined search, ordering, etc.)

---
+++ listreports_ssp.php
@@ -0,0 +1,121 @@
+<?php
+	/* 		
+		*
+		* Vulkan hardware capability database server implementation
+		*	
+		* Copyright (C) 2016 by Sascha Willems (www.saschawillems.de)
+		*	
+		* This code is free software, you can redistribute it and/or
+		* modify it under the terms of the GNU Affero General Public
+		* License version 3 as published by the Free Software Foundation.
+		*	
+		* Please review the following information to ensure the GNU Lesser
+		* General Public License version 3 requirements will be met:
+		* http://www.gnu.org/licenses/agpl-3.0.de.html
+		*	
+		* The code is distributed WITHOUT ANY WARRANTY; without even the
+		* implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+		* PURPOSE.  See the GNU AGPL 3.0 for more details.		
+		*
+	*/
+
+	include './dbconfig.php';
+	include './header.inc';	
+	include './functions.php';	
+
+	dbConnect();
+
+?>
+
+<center>
+	<div class="tablediv">	
+		<form method="get" action="compare.php?compare">	
+		<table id='reports' class='table table-striped table-bordered table-hover responsive' style='width:auto;'>
+			<thead>
+				<tr>
+					<th></th>
+					<th>device</th>
+					<th>driver</th>
+					<th>api</th>
+					<th>vendor</th>
+					<th>type</th>
+					<th>os</thth>
+					<th>version</th>
+					<th>platform</th>
+					<th><input type='submit' name='compare' value='compare' class='button'></th>
+				</tr>
+				<tr>
+					<td>id</td>
+					<td>Device</td>
+					<td>Driver</td>
+					<td>Api</td>
+					<td>Vendor</td>
+					<td>Type</td>
+					<td>OS</tdth>
+					<td>Version</td>
+					<td>Platform</td>
+					<td>Compare</td>
+				</tr>
+			</thead>		
+		</table>
+		</form>
+	</div>
+</center>
+
+<script>
+	$( document ).ready(function() {
+
+		var table = $('#reports').DataTable({
+			"processing": true,
+			"serverSide": true,
+			"paging" : true,		
+			"searching": true,	
+			"dom": 'lrtip',	
+			"pageLength" : 25,		
+			"order": [[ 0, 'desc' ]],
+			"ajax": {
+				url :"responses/listreports.php",
+				type: "post",
+			},
+			"aoColumns": [
+				{ mData: 'id' },
+				{ mData: 'device' },
+				{ mData: 'driver' },
+				{ mData: 'api' },
+				{ mData: 'vendor' },
+				{ mData: 'type' },
+				{ mData: 'os' },
+				{ mData: 'version' },
+				{ mData: 'platform' },
+				{ mData: 'compare' },
+			],
+			// Pass order by column information to server side script
+			fnServerParams: function(data) {
+				data['order'].forEach(function(items, index) {
+					data['order'][index]['column'] = data['columns'][items.column]['data'];
+				});
+			},
+		});   
+
+		// Per-Column filter boxes
+		$('#reports thead th').each( function (i) {
+			if ((i != 0) && (i != 9)) {
+				var title = $('#reports thead th').eq( $(this).index() ).text();
+				var w = (i > 1) ? 120 : 240;
+				$(this).html( '<input type="text" placeholder="'+title+'" data-index="'+i+'" style="width: '+w+'px;" class="filterinput" />' );
+			}
+		}); 
+		$(table.table().container() ).on('keyup', 'thead input', function () {
+			table
+				.column($(this).data('index'))
+				.search(this.value)
+				.draw();
+		});		
+
+	});
+</script>
+
+<?php include './footer.inc'; ?>
+
+</body>
+</html>
---
+++ responses/listreports.php
@@ -0,0 +1,106 @@
+<?php
+	/* 		
+		*
+		* Vulkan hardware capability database server implementation
+		*	
+		* Copyright (C) 2016-2017 by Sascha Willems (www.saschawillems.de)
+		*	
+		* This code is free software, you can redistribute it and/or
+		* modify it under the terms of the GNU Affero General Public
+		* License version 3 as published by the Free Software Foundation.
+		*	
+		* Please review the following information to ensure the GNU Lesser
+		* General Public License version 3 requirements will be met:
+		* http://www.gnu.org/licenses/agpl-3.0.de.html
+		*	
+		* The code is distributed WITHOUT ANY WARRANTY; without even the
+		* implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+		* PURPOSE.  See the GNU AGPL 3.0 for more details.		
+		*
+	*/
+
+    include '../dbconfig.php';
+    include '../functions.php';
+
+    $data = array();
+
+    DB::connect();
+             
+    $orderByColumn = $_REQUEST['order'][0]['column'];
+    $orderByDir = $_REQUEST['order'][0]['dir'];
+
+    $searchColumns = array('id', 'p.devicename', 'p.driverversion', 'p.apiversion', 'vendor', 'p.devicetype', 'r.osname', 'r.osversion', 'r.osarchitecture');
+    $search = $_REQUEST['columns'][1]['search']['value'];
+    $whereClause = '';
+
+    $filters = array();
+    for ($i = 0; $i < sizeof($_REQUEST['columns']); $i++) {
+        $column = $_REQUEST['columns'][$i];
+        if ($column['search']['value'] != '') {
+            $filters[] = $searchColumns[$i]." like '%".$column['search']['value']."%'";
+        }
+    }
+    if (sizeof($filters) > 0) {
+        $searchClause = 'having '.implode(' and ', $filters);
+    }
+
+    $sql = "select 
+        r.id,
+        p.devicename as device,
+        ifnull(p.driverversionraw, p.driverversion) as driver,
+        p.driverversion,
+        p.vendorid,
+        p.apiversion as api,
+        VendorId(p.vendorid) as vendor,
+        p.devicetype,
+        r.osname,
+        r.osversion,
+        r.osarchitecture
+        from reports r
+        left join
+        deviceproperties p on (p.reportid = r.id)
+        ".$searchClause."                
+        order by ".$orderByColumn." ".$orderByDir;
+    $paging = "LIMIT ".$_REQUEST["length"]. " OFFSET ".$_REQUEST["start"];
+
+    $devices = DB::$connection->prepare($sql." ".$paging);
+    $devices->execute();
+    if ($devices->rowCount() > 0) { 
+        foreach ($devices as $device) {
+            $driver = getDriverVerson($device["driver"], "", $device["vendorid"]);
+            $data[] = array(
+                'id' => $device["id"], 
+                'device' => '<a href="displayreport.php?id='.$device["id"].'">'.$device["device"].'</a>', 
+                'driver' => $device["driverversion"], 
+                'api' => $device["api"], 
+                'vendor' => $device["vendor"],
+				'type' => strtolower(str_replace('_GPU', '', $device["devicetype"])),
+				'os' => $device["osname"],
+				'version' => $device["osversion"],
+				'platform' => $device["osarchitecture"],
+                'compare' => '<center><input type="checkbox" name="id['.$device["id"].']"></center>'
+            );
+        }        
+    }
+
+    $stmnt = DB::$connection->prepare("select count(*) from reports");
+    $stmnt->execute();
+    $totalCount = $stmnt->fetchColumn(); 
+
+    $filteredCount = $totalCount;
+    if ($searchClause != '') {
+        $stmnt = DB::$connection->prepare($sql);
+        $stmnt->execute();
+        $filteredCount = $stmnt->rowCount();     
+    }
+
+    $results = array(
+        "draw" => isset($_REQUEST['draw']) ? intval( $_REQUEST['draw'] ) : 0,        
+        "recordsTotal" => intval($totalCount),
+        "recordsFiltered" => intval($filteredCount),
+        "data" => $data);
+
+    DB::disconnect();     
+
+    echo json_encode($results);
+?>
