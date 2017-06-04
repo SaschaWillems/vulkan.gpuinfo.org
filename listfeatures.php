1f9fe9651e774ed9976550a9c109f793b2d38208
saschawillems <webmaster@saschawillems.de>
2017-06-04 14:00:32 +0200

    Feature listing page now uses PDO

--- listfeatures.php
+++ listfeatures.php
@@ -3,7 +3,7 @@
 		*
 		* Vulkan hardware capability database server implementation
 		*	
-		* Copyright (C) 2016 by Sascha Willems (www.saschawillems.de)
+		* Copyright (C) 2016-2017 by Sascha Willems (www.saschawillems.de)
 		*	
 		* This code is free software, you can redistribute it and/or
 		* modify it under the terms of the GNU Affero General Public
@@ -21,15 +21,21 @@
 	
 	include './dbconfig.php';
 	include './header.inc';	
-	
-	dbConnect();	
 ?>
 	
-<style>
-	.dataTables_filter {
-		display: none;
-	}
-</style>
+<script>
+	$(document).ready(function() {
+		var table = $('#features').DataTable({
+			"pageLength" : -1,
+			"paging" : false,
+			"stateSave": false, 
+			"searchHighlight" : true,	
+			"dom": 'f',			
+			"bInfo": false,	
+			"order": [[ 0, "asc" ]]	
+		});
+	} );	
+</script>
 
 <div class='header'>
 	<h4 style='margin-left:10px;'>Listing device features</h4>
@@ -37,61 +43,53 @@
 
 <center>	
 
-	<div class="tablediv">
-
-	<?php include ("filter.php"); ?>
+	<div class='parentdiv'>
+	<div class='tablediv' style='width:auto; display: inline-block;'>
 
 	<table id="features" class="table table-striped table-bordered table-hover responsive" style='width:auto;'>
 		<thead>
 			<tr>				
-				<td class="caption">Feature</td>
-				<td class="caption">Supported</td>
-				<td class="caption">Unsupported</td>
+				<td>Feature</td>
+				<td>Supported</td>
+				<td>Unsupported</td>
 			</tr>
 		</thead>
-		<tbody>
-		
-		<?php		
-			$reportCount = mysql_result(mysql_query("select count(*) from reports"), 0);
-			$sqlresult = mysql_query("select COLUMN_NAME from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'devicefeatures' and COLUMN_NAME not in ('reportid')") or die(mysql_error());  	
-			$columns = array();
-			while($row = mysql_fetch_row($sqlresult))
-			{
-				$count = mysql_result(mysql_query("select count(*) from devicefeatures where `".$row[0]."` = 1"), 0);  	
-				$supported = number_format((($count/$reportCount)*100.0), 0);
-				$unsupported = number_format(100.0 - (($count/$reportCount)*100.0), 0);
-				echo "<tr>";
-				echo "<td>".$row[0]."</td>";
-				echo "<td><a class='supported' href='listreports.php?feature=".$row[0]."'>".$supported."%</a></td>";
-				echo "<td><a class='unsupported' href='listreports.php?feature=".$row[0]."&option=not'>".$unsupported."%</a></td>";
-				echo "</tr>";
-			}
-						
-			dbDisconnect();	
-		?>   
-		
+		<tbody>		
+			<?php	
+				DB::connect();
+				try {
+					$res =DB::$connection->prepare("select count(*) from reports"); 
+					$res->execute(); 
+					$reportCount = $res->fetchColumn(); 
+
+					$sql = "select COLUMN_NAME from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'devicefeatures' and COLUMN_NAME not in ('reportid')";
+					$features = DB::$connection->prepare($sql);
+					$features->execute();
+
+					if ($features->rowCount() > 0) { 
+						while ($feature = $features->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {								
+							$stmnt = DB::$connection->prepare("select count(*) from devicefeatures where `".$feature[0]."` = 1");
+							$stmnt->execute();
+							$count = $stmnt->fetchColumn();
+							$supported = number_format((($count/$reportCount)*100.0), 0);
+							$unsupported = number_format(100.0 - (($count/$reportCount)*100.0), 0);
+							echo "<tr>";
+							echo "<td>".$feature[0]."</td>";
+							echo "<td><a class='supported' href='listreports.php?feature=".$feature[0]."'>".$supported."%</a></td>";
+							echo "<td><a class='unsupported' href='listreports.php?feature=".$feature[0]."&option=not'>".$unsupported."%</a></td>";
+							echo "</tr>";
+						}
+					}
+
+				} catch (PDOException $e) {
+					echo "<b>Error while fetcthing data!</b><br>";
+				}
+				DB::disconnect();			
+			?>   
 		</tbody>
 	</table>  
 
-	<script>
-		$(document).ready(function() 
-		{
-			var table = $('#features').DataTable({
-				"pageLength" : -1,
-				"paging" : false,
-				"stateSave": false, 
-				"searchHighlight" : true,
-				"bInfo": false,
-				"sDom": 'flipt',
-			});
-
-			$("#searchbox").on("keyup search input paste cut", function() {
-				table.search(this.value).draw();
-			});  
-
-		} );	
-	</script>
-
+	</div>
 	</div>
 	
 <?php include './footer.inc'; ?>
