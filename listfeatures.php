0378ec320b62e08356a1de77b3a8f850fd810b6c
Sascha Willems <webmaster@saschawillems.de>
2019-07-07 13:08:30 +0200

    Changed feature listing to per-platform device coverage

--- listfeatures.php
+++ listfeatures.php
@@ -3,7 +3,7 @@
 		*
 		* Vulkan hardware capability database server implementation
 		*	
-		* Copyright (C) 2016-2017 by Sascha Willems (www.saschawillems.de)
+		* Copyright (C) Sascha Willems (www.saschawillems.de)
 		*	
 		* This code is free software, you can redistribute it and/or
 		* modify it under the terms of the GNU Affero General Public
@@ -32,7 +32,10 @@
 			"searchHighlight" : true,	
 			"dom": 'f',			
 			"bInfo": false,	
-			"order": [[ 0, "asc" ]]	
+			"order": [[ 0, "asc" ]],
+			"columnDefs": [{
+      			"targets": [ 1, 2, 3 ]
+			}]			
 		});
 	} );	
 </script>
@@ -43,53 +46,79 @@
 
 <center>	
 
-	<div class='parentdiv'>
 	<div class='tablediv' style='width:auto; display: inline-block;'>
 
 	<table id="features" class="table table-striped table-bordered table-hover responsive" style='width:auto;'>
 		<thead>
+			<tr>			
+				<th></th>
+				<th colspan=3 style="text-align: center;">Device coverage</th>
+			</tr>
 			<tr>				
 				<td>Feature</td>
-				<td>Supported</td>
-				<td>Unsupported</td>
+				<th style="text-align: center; width:90px;">Windows</th>
+				<th style="text-align: center; width:90px;">Linux</th>
+				<th style="text-align: center; width:90px;">Android</th>
 			</tr>
 		</thead>
 		<tbody>		
 			<?php	
 				DB::connect();
 				try {
-					$res =DB::$connection->prepare("select count(*) from reports"); 
+					$res =DB::$connection->prepare("SELECT count(*) from reports"); 
 					$res->execute(); 
 					$reportCount = $res->fetchColumn(); 
 
-					$sql = "select COLUMN_NAME from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'devicefeatures' and COLUMN_NAME not in ('reportid')";
-					$features = DB::$connection->prepare($sql);
-					$features->execute();
+					$viewDeviceCount =DB::$connection->prepare("SELECT * from viewDeviceCount");
+					$viewDeviceCount->execute(); 
+					$deviceCounts = $viewDeviceCount->fetch(PDO::FETCH_ASSOC);
 
-					if ($features->rowCount() > 0) { 
-						while ($feature = $features->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {								
-							$stmnt = DB::$connection->prepare("select count(*) from devicefeatures where `".$feature[0]."` = 1");
-							$stmnt->execute();
-							$count = $stmnt->fetchColumn();
-							$supported = number_format((($count/$reportCount)*100.0), 0);
-							$unsupported = number_format(100.0 - (($count/$reportCount)*100.0), 0);
-							echo "<tr>";
-							echo "<td>".$feature[0]."</td>";
-							echo "<td><a class='supported' href='listreports.php?feature=".$feature[0]."'>".$supported."%</a></td>";
-							echo "<td><a class='unsupported' href='listreports.php?feature=".$feature[0]."&option=not'>".$unsupported."%</a></td>";
-							echo "</tr>";
-						}
+					// Collect feature column names
+					$sql = "SELECT COLUMN_NAME from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'devicefeatures' and COLUMN_NAME not in ('reportid')";
+					$stmnt = DB::$connection->prepare($sql);
+					$stmnt->execute();
+
+					$features = array();
+					$sqlColumns = "";
+					while ($row = $stmnt->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
+						$features[] = $row[0];
+						$sqlColumns .= "max(".$row[0].") as $row[0],";
 					}
 
+					// Get supported counts per platform
+					$supportedCounts[0] = array();
+					$supportedCounts[1] = array();
+					$supportedCounts[2] = array();
+					for ($i = 0; $i < 3; $i++) {
+						$stmnt = DB::$connection->prepare(
+							"SELECT ifnull(r.displayname, dp.devicename) as device, "
+							.substr($sqlColumns, 0, -1).
+							" FROM devicefeatures df join deviceproperties dp on dp.reportid = df.reportid join reports r on r.id = df.reportid where r.ostype = ".(integer)$i." group by device");
+						$stmnt->execute();
+						while ($row = $stmnt->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT)) {
+							foreach($row as $key => $value) {
+								if (strcasecmp($key, 'device') != 0) {
+									$supportedCounts[$i][$key] += $value;
+								}
+							}
+						}
+					}
+					foreach($features as $feature) {
+						echo "<tr>";
+						echo "<td>".$feature."</td>";
+						echo "<td class='text-center'><a href=\"listdevicescoverage.php?platform=windows&feature=$feature\">".round($supportedCounts[0][$feature]/$deviceCounts["windows"]*100, 2)."%</a></td>";
+						echo "<td class='text-center'><a href=\"listdevicescoverage.php?platform=linux&feature=$feature\">".round($supportedCounts[1][$feature]/$deviceCounts["linux"]*100, 2)."%</a></td>";
+						echo "<td class='text-center'><a href=\"listdevicescoverage.php?platform=android&feature=$feature\">".round($supportedCounts[2][$feature]/$deviceCounts["android"]*100, 2)."%</a></td>";
+						echo "</tr>";
+					}
 				} catch (PDOException $e) {
-					echo "<b>Error while fetcthing data!</b><br>";
+					echo "<b>Error while fetching data!</b><br>";
 				}
 				DB::disconnect();			
 			?>   
 		</tbody>
 	</table>  
 
-	</div>
 	</div>
 	
 <?php include './footer.inc'; ?>
