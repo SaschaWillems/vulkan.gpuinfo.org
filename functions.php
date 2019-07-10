6d4d5c43e221e833481c18b1ceb1a012e422be57
Petr Kraus <petr_kraus@email.cz>
2019-07-10 18:40:36 +0200

    Support new present mode strings

--- functions.php
+++ functions.php
@@ -218,6 +218,8 @@ function getPresentMode($value)
 		"MAILBOX_KHR" => 1,
 		"FIFO_KHR" => 2,
 		"FIFO_RELAXED_KHR" => 3,
+		"SHARED_DEMAND_REFRESH_KHR" => 1000111000,
+		"SHARED_CONTINUOUS_REFRESH_KHR" => 1000111001,
 	);
 	if (in_array($value, $modes))
 	{
