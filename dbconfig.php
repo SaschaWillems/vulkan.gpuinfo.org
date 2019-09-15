6a2e77b2143a6b729f5d00613a79ad738f88d651
Sascha Willems <webmaster@saschawillems.de>
2019-09-15 18:08:37 +0200

    Added database class, credentials moved to separate file

---
+++ dbconfig.php
@@ -0,0 +1,54 @@
+<?php
+    include "dbsettings.php";
+
+	$bgcolordef = '#FFFFFF';	    
+	$bgcolorodd = '#EFEFEF';	 	
+	$bgcolorhead = '#CFCFCF';	 		
+	
+    $db_link = false;
+    
+    function dbConnect()
+    {
+        global $db_link;
+        if ($db_link) {
+			return $db_link;
+		}
+        $db_link = mysql_connect(DBHOST, DBUSER, DBPASSWORD) or die('Could not connect to database');
+        mysql_select_db(DBNAME, $db_link) or die('Database selection failed');
+        return $db_link;
+    }
+    
+    function dbDisconnect()
+    {
+        global $db_link;
+        if(!$db_link) {
+            mysql_close($db_link);
+		}
+        $db_link = false;
+    }	
+
+    // PDO
+    class DB {
+        public static $connection = null;
+
+        public static function connect() {
+            $DBNAME = DBNAME;
+            $DBHOST = DBHOST;
+            try {
+                DB::$connection = new PDO("mysql:dbname=$DBNAME;host=$DBHOST", DBUSER, DBPASSWORD);
+                DB::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                DB::$connection = null;
+                die($e->getMessage());
+            }                
+        }
+        public static function disconnect() {
+            DB::$connection = null;
+        }
+        public static function getCount($statement, $params) {
+            $stmnt = DB::$connection->prepare($statement);
+            $stmnt->execute($params);				
+            return $stmnt->fetchColumn();
+        }        
+    }        
+?>
