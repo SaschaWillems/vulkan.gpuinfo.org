4ffac5bf1d72a99237e4a5b482d6b2dfbdec64d5
Sascha Willems <webmaster@saschawillems.de>
2019-12-11 19:16:52 +0100

    Page generator class, header and footer as php files

---
+++ page_generator.php
@@ -0,0 +1,35 @@
+<?php
+	/* 		
+		*
+		* Vulkan hardware capability database server implementation
+		*	
+		* Copyright (C) by Sascha Willems (www.saschawillems.de)
+		*	
+		* This code is free software, you can redistribute it and/or
+		* modify it under the terms of the GNU Affero General Public
+		* License version 3 as published by the Free Software Foundation.
+		*	
+		* Please review the following information to ensure the GNU Lesser
+		* General Public License version 3 requirements will be met:
+		* http://www.gnu.org/licenses/agpl-3.0.de.html
+		*	
+		* The code is distributed WITHOUT ANY WARRANTY; without even the
+		* implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+		* PURPOSE.  See the GNU AGPL 3.0 for more details.		
+		*
+    */
+    
+    class PageGenerator {
+
+        public static function header($title = null) {
+            $page_title = $title;
+            include './header.php';
+        }
+
+        public static function footer() {
+            include './footer.php';
+        }
+
+    }
+
+?>
