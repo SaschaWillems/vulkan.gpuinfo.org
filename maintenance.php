6488339df53cb99c692ed5ec99fcfff45e135a1a
Sascha Willems <webmaster@saschawillems.de>
2020-01-10 10:29:33 +0100

    Maintenance page

---
+++ maintenance.php
@@ -0,0 +1,6 @@
+<div style="position: absolute; left: 50%; top: 50%; -webkit-transform: translate(-50%, -50%); transform: translate(-50%, -50%);">
+    <center>
+        <img src="./images/vulkan48.png">
+        <h2>The Vulkan hardware database is down for maintenance</h2>
+    </center>
+</div>
