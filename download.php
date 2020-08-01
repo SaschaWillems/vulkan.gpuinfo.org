ce748d045f17043cabe74cc2ef9e52b1e9fffa3f
Sascha Willems <webmaster@saschawillems.de>
2020-08-01 20:45:48 +0200

    Current release info (2.1 beta)

--- download.php
+++ download.php
@@ -32,65 +32,48 @@
 				The Vulkan Hardware Capability Viewer is open source, you can always build the most recent version yourself using the sources from <a href="https://github.com/SaschaWillems/VulkanCapsViewer">https://github.com/SaschaWillems/VulkanCapsViewer</a>.<br>
 			</div>
 			<div class="page-header">
-				<h3>Current Version (1.4)</h3>
+				<h3>Current stable release (2.03)</h3>
 				<ul>
-					<li><a href="downloads/vulkancapsviewer_1_4_win64.zip">Windows x86-64</a></li>
-					<li><a href="downloads/vulkancapsviewer_1_4_linux64.tar.gz">Linux x86-64</a></li>
+					<li><a href="downloads/vulkancapsviewer_2_03_win64.zip">Windows x86-64</a></li>
+					<li><a href="downloads/vulkancapsviewer_2_03_linux64.tar.gz">Linux x86-64</a> (AppImage)</li>
 					<li>Android (<a href="https://play.google.com/store/apps/details?id=de.saschawillems.vulkancapsviewer">Google Playstore</a>)
 					<ul>
-						<li><a href="downloads/vulkancapsviewer_1_4_arm.apk">Android arm-v7 Beta</a></li>
-						<li><a href="downloads/vulkancapsviewer_1_4_x86.apk">Android x86</a></li>
+						<li><a href="downloads/vulkancapsviewer_2_03_arm.apk">Android arm-v8</a></li>
 					</ul>
 				</ul>
 			</div>			
 			<div class="page-header">
-				<h3>Changelog</h3>
-				<h4>1.4 - 2017-05-28</h4>
+				<h3>Release notes</h3>
+				<h4>2.03 - 2019-12-02</h4>
 					<ul>
 						<li>Added support for new features and properties provided via&nbsp;VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2
-					<ul>
-						<li>Currently supported:
-					<ul>
-						<li>VK_KHR_push_descriptor</li>
-						<li>VK_EXT_discard_rectangles</li>
-						<li>VK_KHX_multiview</li>
-						<li>VK_NVX_multiview_per_view_attributes</li>
-					</ul>
+						<ul>
+							<li>VK_KHR_performance_query</li>
+							<li>VK_KHR_separate_depth_stencil_layouts</li>
+							<li>VK_KHR_buffer_device_address</li>
+						</ul>
 					</li>
 					</ul>
+			</div>
+			<div class="page-header">
+				<h3>Current beta release (2.1)</h3>
+				<ul>
+					<li><a href="downloads/vulkancapsviewer_2_1_beta_win64.zip">Windows x86-64</a></li>
+				</ul>
+			</div>			
+			<div class="page-header">
+				<h3>Release notes</h3>
+				<h4>2.1 - 2020-03-17</h4>
+					<ul>
+						<li>Added support for new features and properties provided via&nbsp;VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2
+						<ul>
+							<li>VK_KHR_ray_tracing</li>
+							<li>VK_EXT_pipeline_creation_cache_control</li>
+							<li>VK_NV_device_generated_commands</li>
+							<li>VK_NV_device_diagnostics_config</li>
+						</ul>
+						<li>Several tweaks for formatting display values</li>
 					</li>
-						<li>Reports can now be saved to disk via command line using the -s argument (without invoking the UI)
-					<ul>
-						<li>Example : vulkanCapsViewer -s my_device.json</li>
-					</ul>
-					</li>
-						<li>Added pipeline cache UUID</li>
-						<li>Exported JSON is now compatible with vkjson_info from LunarG SDK</li>
-						<li>Added Android platform info
-					<ul>
-						<li>Device model and manufacturer</li>
-						<li>Build information</li>
-					</ul>
-					</li>
-						<li>UI improvements&nbsp;
-					<ul>
-						<li>Updated layout and UI scaling&nbsp;</li>
-						<li>Support for high DPI scaling</li>
-						<li>Better touch support for mobile devices</li>
-					</ul>
-					</li>
-					</ul>
-				<hr>
-				<h4>Beta 1.2 - 2016-11-19</h4>
-					<ul>
-						<li><b>New feature: </b> Added support for os-specific surface capabilities</li>
-					</ul>
-				<hr>
-				<h4>Beta 1.1 - 2016-04-03</h4>
-					<ul>
-						<li><b>Fixed : </b>Memory type flags now displayed correct</li>
-						<li><b>Fixed : </b>Assign queue priorities (fixes potential crashes)</li>
-						<li>Display and upload API driver version (integer)</li>
 					</ul>
 			</div>
 		</div>    
