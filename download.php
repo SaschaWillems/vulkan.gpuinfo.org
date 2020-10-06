5a229af15d3bc4fe69789b65010215c936a80df8
Sascha Willems <webmaster@saschawillems.de>
2020-10-06 10:10:26 +0200

    Added Mac OSX download

--- download.php
+++ download.php
@@ -18,7 +18,8 @@
 		* PURPOSE.  See the GNU AGPL 3.0 for more details.		
 		*
 	*/
-	include './header.inc';	
+	include 'page_generator.php';
+	PageGenerator::header('Download');
 ?>
 
 <div id='reportdiv'>	   
@@ -31,65 +32,66 @@
 				The Vulkan Hardware Capability Viewer is open source, you can always build the most recent version yourself using the sources from <a href="https://github.com/SaschaWillems/VulkanCapsViewer">https://github.com/SaschaWillems/VulkanCapsViewer</a>.<br>
 			</div>
 			<div class="page-header">
-				<h3>Current Version (1.4)</h3>
+				<h3>Current stable release 2.21</h3>
 				<ul>
-					<li><a href="downloads/vulkancapsviewer_1_4_win64.zip">Windows x86-64</a></li>
-					<li><a href="downloads/vulkancapsviewer_1_4_linux64.tar.gz">Linux x86-64</a></li>
-					<li>Android (<a href="https://play.google.com/store/apps/details?id=de.saschawillems.vulkancapsviewer">Google Playstore</a>)
+					<li><a href="downloads/vulkancapsviewer_2_21_win64.zip">Windows x86-64 (zip)</a></li>
+					<li>Linux
 					<ul>
-						<li><a href="downloads/vulkancapsviewer_1_4_arm.apk">Android arm-v7 Beta</a></li>
-						<li><a href="downloads/vulkancapsviewer_1_4_x86.apk">Android x86</a></li>
+						<li><a href="downloads/vulkancapsviewer_2_21_linux64.AppImage">X11 x86-64</a> (AppImage)</li>
+						<li><a href="downloads/vulkancapsviewer_2_21_linux64_wayland.AppImage">Wayland x86-64</a> (AppImage)</li>
 					</ul>
+					</li>
+					<li>Android
+					<ul>
+						<li><a href="downloads/vulkancapsviewer_2_21_arm.apk">Android arm-v8 (apk)</a></li>
+					</ul>
+					<li><a href="downloads/vulkancapsviewer_2_21_osx.dmg">Mac OSX (dmg)</a></li>
 				</ul>
 			</div>			
 			<div class="page-header">
-				<h3>Changelog</h3>
-				<h4>1.4 - 2017-05-28</h4>
+				<h3>Release notes</h3>
+				<h4>2.21 - 2020-09-26</h4>
 					<ul>
-						<li>Added support for new features and properties provided via&nbsp;VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2
+						<li>Added support for new features and properties provided via&nbsp;VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2:
+							<ul>
+								<li>VK_KHR_portability_subset</li>
+								<li>VK_EXT_4444_formats</li>
+							</ul>
+						</li>
+					</ul>				
+				<h4>2.2 - 2020-08-02</h4>
 					<ul>
-						<li>Currently supported:
-					<ul>
-						<li>VK_KHR_push_descriptor</li>
-						<li>VK_EXT_discard_rectangles</li>
-						<li>VK_KHX_multiview</li>
-						<li>VK_NVX_multiview_per_view_attributes</li>
+						<li>Added support for new features and properties provided via&nbsp;VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2:
+							<ul>
+								<li>VK_EXT_fragment_density_map2</li>
+								<li>VK_EXT_shader_atomic_float</li>
+								<li>VK_EXT_extended_dynamic_state</li>
+								<li>VK_EXT_private_data</li>
+								<li>VK_EXT_image_robustness</li>
+							</ul>
+						</li>
 					</ul>
+				<h4>2.1 - 2020-03-17</h4>
+					<ul>
+						<li>Added support for new features and properties provided via&nbsp;VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2:
+							<ul>
+								<li>VK_KHR_ray_tracing</li>
+								<li>VK_EXT_pipeline_creation_cache_control</li>
+								<li>VK_NV_device_generated_commands</li>
+								<li>VK_NV_device_diagnostics_config</li>
+							</ul>
+							<li>Several tweaks for formatting display values</li>
+						</li>
+					</ul>
+				<h4>2.03 - 2019-12-02</h4>
+					<ul>
+						<li>Added support for new features and properties provided via&nbsp;VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2:
+						<ul>
+							<li>VK_KHR_performance_query</li>
+							<li>VK_KHR_separate_depth_stencil_layouts</li>
+							<li>VK_KHR_buffer_device_address</li>
+						</ul>
 					</li>
-					</ul>
-					</li>
-						<li>Reports can now be saved to disk via command line using the -s argument (without invoking the UI)
-					<ul>
-						<li>Example : vulkanCapsViewer -s my_device.json</li>
-					</ul>
-					</li>
-						<li>Added pipeline cache UUID</li>
-						<li>Exported JSON is now compatible with vkjson_info from LunarG SDK</li>
-						<li>Added Android platform info
-					<ul>
-						<li>Device model and manufacturer</li>
-						<li>Build information</li>
-					</ul>
-					</li>
-						<li>UI improvements&nbsp;
-					<ul>
-						<li>Updated layout and UI scaling&nbsp;</li>
-						<li>Support for high DPI scaling</li>
-						<li>Better touch support for mobile devices</li>
-					</ul>
-					</li>
-					</ul>
-				<hr>
-				<h4>Beta 1.2 - 2016-11-19</h4>
-					<ul>
-						<li><b>New feature: </b> Added support for os-specific surface capabilities</li>
-					</ul>
-				<hr>
-				<h4>Beta 1.1 - 2016-04-03</h4>
-					<ul>
-						<li><b>Fixed : </b>Memory type flags now displayed correct</li>
-						<li><b>Fixed : </b>Assign queue priorities (fixes potential crashes)</li>
-						<li>Display and upload API driver version (integer)</li>
 					</ul>
 			</div>
 		</div>    
@@ -97,7 +99,7 @@
 </div>
 
 <?php
-	include './footer.inc';
+	PageGenerator::footer();
 ?>
 
 </body>
