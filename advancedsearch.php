3705c4ddbe92a4ee374df2dd545ff8e4b84387ee
Sascha Willems <webmaster@saschawillems.de>
2020-10-11 08:13:43 +0200

    Removed no-longer used code

--- advancedsearch.php
+++ advancedsearch.php
@@ -143,77 +143,7 @@
 
     echo "  </div>";
     echo "</div>";
-  }
-
-  function generateSearchGroup(string $name) {
-    global $search_groups;
-    $group = $search_groups[$name];
-    assert($group);
-    echo '<h3>'.$group['caption'].'</h3>';
-    foreach ($group['subjects'] as $search) {
-      $format_selection = strpos($search['id'], 'format_') === 0;
-  ?>
-      <form class="form-horizontal" style="padding-top: 25px;" method="get" action="./listdevices.php">
-        <div class="form-group" style="margin: 0;">
-
-        <div class="panel panel-default">
-          <div class="panel-heading"><b><?= $search['subject'] ?></b></div>
-          <div class="panel-body">
-            <?php
-              if ($format_selection) {
-                echo "<div style='margin-bottom: 5px;'>Select format:</div>";
-                generateFormatSelection();
-                echo "<div style='margin-top:15px; margin-bottom: 5px;'>Select feature flags:</div>";
-              }
-              switch ($search['type']) {
-                case 'select':
-                  echo '<select class="form-control" id="'.$search['id'].'" name="'.$search['id'].'">';
-                  echo '<option></option>';
-                  foreach ($search['options'] as $value => $text) {
-                    echo '<option value="'.$value.'">'.$text.'</option>';
-                  }
-                  echo '</select>';
-                  break;
-                case 'select_database':
-                  DB::connect();
-                  try {
-                    $stmnt = DB::$connection->prepare($search['options_statement']);
-                    $stmnt->execute();
-                    echo '<select class="form-control" id="'.$search['id'].'" name="'.$search['id'].'">';
-                    echo '<option></option>';
-                    foreach ($stmnt as $row) {
-                      echo '<option value="'.$row[0].'">'.$row[0].'</option>';
-                    }
-                    echo '</select>';
-                  } catch (Throwable $e) {
-                    echo "<span style='color:red;'>Error: Could not retrieve values from database!</span>";
-                  }
-                  DB::disconnect();
-                  break;
-                case 'select_list':
-                  echo '<select multiple class="form-control" id="'.$search['id'].'" name="'.$search['id'].'[]" size="'.count($search["options"]).'">';
-                  foreach ($search['options'] as $value => $text) {
-                    echo '<option value="'.$value.'">'.$text.'</option>';
-                  }
-                  echo '</select>';
-                  break;
-                case 'number':
-                  echo '<input class="form-control" id="'.$search['id'].'" name="'.$search['id'].'" type="number">';
-                  break;
-              }
-            ?>
-          </div>
-          <div class="panel-footer text-right">
-            <button type="submit" name="advancedsearch" value="1" class="btn btn-primary">Search</button>
-          </div>
-        </div>
-
-      </div>
-     </form>
-  <?php
-    }
-  }
-
+  } 
 ?>
 
 <div class='header'>
