5d477068b2871bd48e155fc522be3fdfc4039fd3
Sascha Willems <webmaster@saschawillems.de>
2020-10-17 15:20:26 +0200

    Beginning to move global functions into report class

---
+++ report.class.php
@@ -0,0 +1,108 @@
+<?php
+	/* 		
+		*
+		* Vulkan hardware capability database server implementation
+		*	
+		* Copyright (C) 2016-2020 by Sascha Willems (www.saschawillems.de)
+		*	
+		* This code is free software, you can redistribute it and/or
+		* modify it under the terms of the GNU Affero General Public
+		* License version 3 as published by the Free Software Foundation.
+		*	
+		* Please review the following information to ensure the GNU Lesser
+		* General Public License version 3 requirements will be met:
+		* http://www.gnu.org/licenses/agpl-3.0.de.html
+		*	
+		* The code is distributed WITHOUT ANY WARRANTY; without even the
+		* implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+		* PURPOSE.  See the GNU AGPL 3.0 for more details.		
+		*
+    */
+  
+    class ReportFlags {
+        public $has_instance_data = false;
+        public $has_surface_caps = false;
+        public $has_platform_details = false;
+        public $has_extended_features = false;
+        public $has_extended_properties = false;
+        public $has_vulkan_1_1_features = false;
+        public $has_vulkan_1_1_properties = false;
+        public $has_vulkan_1_2_features = false;
+        public $has_vulkan_1_2_properties = false;
+    }
+
+    class Report {        
+        public $id = null;
+        public ReportFlags $flags;
+
+        function __construct($reportid)
+        {
+            $this->id = $reportid;
+            $this->flags = new ReportFlags;
+        }
+
+        public function fetchData() 
+        {
+            // Counters (for badges)
+            // Flags for optional data
+            DB::connect();
+            $this->flags->has_instance_data =  DB::getCount("SELECT (select count(*) from deviceinstanceextensions where reportid = :reportid) + (select count(*) from deviceinstancelayers where reportid = :reportid)", [':reportid' => $this->id]) > 0;
+            $this->flags->has_surface_caps = DB::getCount("SELECT count(*) from devicesurfacecapabilities where reportid = :reportid", [':reportid' => $this->id]) > 0;
+            $this->flags->has_platform_details = DB::getCount("SELECT count(*) from deviceplatformdetails where reportid = :reportid", [':reportid' => $this->id]) > 0;
+            $this->flags->has_extended_features = DB::getCount("SELECT count(*) from devicefeatures2 where reportid = :reportid", [':reportid' => $this->id]) > 0;
+            $this->flags->has_extended_properties = DB::getCount("SELECT count(*) from deviceproperties2 where reportid = :reportid", [':reportid' => $this->id]) > 0;
+            $this->flags->has_vulkan_1_1_features = DB::getCount("SELECT count(*) from devicefeatures11 where reportid = :reportid", [':reportid' => $this->id]) > 0;
+            $this->flags->has_vulkan_1_1_properties = DB::getCount("SELECT count(*) from deviceproperties11 where reportid = :reportid", [':reportid' => $this->id]) > 0;
+            $this->flags->has_vulkan_1_2_features = DB::getCount("SELECT count(*) from devicefeatures12 where reportid = :reportid", [':reportid' => $this->id]) > 0;
+            $this->flags->has_vulkan_1_2_properties = DB::getCount("SELECT count(*) from deviceproperties12 where reportid = :reportid", [':reportid' => $this->id]) > 0;
+            DB::disconnect();
+        }
+
+        public function fetchDeviceInfo()
+        {
+            try {
+                $sql = "SELECT 
+                p.devicename,
+                r.displayname,
+                p.driverversionraw,
+                p.driverversion,
+                p.devicetype,
+                p.apiversion,
+                p.vendorid,
+                VendorId(p.vendorid) as 'vendor',
+                concat('0x', hex(cast(p.deviceid as UNSIGNED))) as 'deviceid',
+                p.pipelineCacheUUID,
+                r.osname,
+                r.osarchitecture,
+                r.osversion,
+                r.submitter,
+                r.submissiondate,
+                r.version as reportversion,
+                r.description,
+                'devsim' as `devsim`
+                from reports r
+                left join
+                deviceproperties p on (p.reportid = r.id)
+                where r.id = :reportid";
+		        $stmnt = DB::$connection->prepare($sql);
+                $stmnt->execute([":reportid" => $this->id]);
+                $result = $stmnt->fetchAll(PDO::FETCH_ASSOC);
+                return $result;
+            } catch (Throwable $e) {
+                return null;
+            }
+        }
+
+        public function fetchPlatformDetails() 
+        {
+            try {
+                $sql = "SELECT name, value from deviceplatformdetails dpfd join platformdetails pfd on dpfd.platformdetailid = pfd.id where dpfd.reportid = :reportid order by name asc";
+                $stmnt = DB::$connection->prepare($sql);
+                $stmnt->execute([":reportid" => $this->id]);
+                $result = $stmnt->fetchAll(PDO::FETCH_ASSOC);
+                return $result;
+            } catch (Throwable $e) {
+                return null;
+            }
+        }
+    }
