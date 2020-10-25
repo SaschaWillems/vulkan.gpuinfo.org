cded848f86331b22f2be409f039cf3a677c76904
Sascha Willems <webmaster@saschawillems.de>
2020-10-25 08:41:11 +0100

    Use report class for displaying memory info
    Combined heaps and types into one view that can be filtered

--- displayreport_memory.php
+++ displayreport_memory.php
@@ -19,91 +19,72 @@
 		*
 	*/	
 ?>	
-	<div>
-		<ul class='nav nav-tabs nav-level1'>
-			<li class='active'><a data-toggle='tab' href='#memorytypes'>Memory types <span class='badge'><?php echo $memtypecount ?></span></a></li>
-			<li><a data-toggle='tab' href='#memoryheaps'>Memory heaps <span class='badge'><?php echo $memheapcount ?></span></a></li>
-		</ul>
-	</div>
-	
-	<div class='tab-content'>
-		<div id='memorytypes' class='tab-pane fade in active reportdiv'>
-<?php	
-	try {
-		$stmnt = DB::$connection->prepare("SELECT * from devicememorytypes where reportid = :reportid");
-		$stmnt->execute(array(":reportid" => $reportID));
-		$index = 0;
-		while($row = $stmnt->fetch(PDO::FETCH_ASSOC)) {
-			echo "<table class='table table-striped table-bordered table-hover responsive' style='width:100%;'>";
-			echo "<thead><tr>";
-			echo "<tr><td colspan=2 class=tablehead>Memory type $index</td></tr>";		
-			echo "</tr></thead><tbody>";				
-			echo "<tr>";
-			echo "<td class='key'>Heapindex</td>";
-			echo "<td>".$row["heapindex"]."</td>";
-			echo "</tr>";	
-			// Flags
-			echo "<tr>";
-			echo "<td class='key'>Flags</td>";
-			echo "<td>";
-			$memoryFlags = getMemoryTypeFlags($row["propertyflags"]);
-			if (sizeof($memoryFlags) > 0) {
-				foreach ($memoryFlags as $flag) {
-					echo $flag."<br>";
+<div id='memorytypes' class='tab-pane fade in active reportdiv'>
+	<table id='devicememory' class='table table-striped table-bordered table-hover reporttable'>
+		<thead>
+			<tr>
+				<td class='caption'>Property</td>
+				<td class='caption'>Value</td>
+				<td></td>
+			</tr>
+		</thead>
+		<tbody>
+			<?php	
+				$data = $report->fetchMemoryHeaps();
+				if ($data) {
+					$heap_index = 0;
+					foreach($data as $memory_heap) {
+						foreach($memory_heap as $key => $value) {			
+							if ($key == 'id') {
+								continue;
+							}
+							echo "<tr>";
+							echo "<td class='subkey' style='width: 25%'>".ucfirst($key)."</td>";
+							echo "<td>";
+							switch($key) {
+								case 'size': 
+									echo number_format($value)." bytes";
+								break;
+								case 'flags':
+									$flags = getMemoryHeapFlags($value);
+									if (sizeof($flags) > 0) {
+										foreach ($flags as $flag) {
+											echo $flag."<br>";
+										}
+									} else {
+										echo "none";
+									}					
+								break;
+								default:
+									echo $value;
+							}
+							echo "</td>";
+							echo "<td>Memory heap $heap_index</td>";
+							echo "</tr>";
+						}
+						$memory_types = $report->fetchMemoryTypes($heap_index);
+						if ($memory_types) {
+							foreach($memory_types as $memory_type_index => $memory_types) {
+								echo "<tr>";
+								echo "<td class='subkey' style='width: 25%'>Memory type $memory_type_index</td>";
+								echo "<td>";
+								$memoryFlags = getMemoryTypeFlags($memory_types['propertyflags']);
+								if (sizeof($memoryFlags) > 0) {
+									foreach ($memoryFlags as $flag) {
+										echo $flag."<br>";
+									}
+								} else {
+									echo "none";
+								}									
+								echo "</td>";
+								echo "<td>Memory heap $heap_index</td>";
+								echo "</tr>";
+							}
+						}
+						$heap_index++;
+					}
 				}
-			} else {
-				echo "none";
-			}
-			echo "<tr>";				
-			echo "</tbody></table>";								
-			$index++;
-		}
-	} catch (Exception $e) {
-		die('Error while fetching report features');
-		DB::disconnect();
-	}
-
-	echo "</div>";	
-	
-?>	
-	<div id='memoryheaps' class='tab-pane fade reportdiv'>
-		<div class="alert alert-warning" role="alert">
-			<b>Note:</b> Listing may contain memory heaps with host sizes!
-		</div>	
-<?php			
-	try {
-		$stmnt = DB::$connection->prepare("SELECT * from devicememoryheaps where reportid = :reportid");
-		$stmnt->execute(array(":reportid" => $reportID));
-		$index = 0;
-		while($row = $stmnt->fetch(PDO::FETCH_ASSOC)) {
-			echo "<table class='table table-striped table-bordered table-hover responsive' style='width:100%;'>";
-			echo "<thead><tr>";
-			echo "<tr><td colspan=2 class=tablehead>Memory heap $index</td></tr>";		
-			echo "</tr></thead><tbody>";				
-			echo "<tr>";
-			echo "<td class='key'>Size</td>";
-			echo "<td>".number_format($row["size"])." bytes</td>";
-			echo "</tr>";	
-			// Flags
-			echo "<tr>";
-			echo "<td class='key'>Flags</td>";
-			echo "<td>";
-			$flags = getMemoryHeapFlags($row["flags"]);
-			if (sizeof($flags) > 0) {
-				foreach ($flags as $flag) {
-					echo $flag."<br>";
-				}
-			} else {
-				echo "none";
-			}
-			echo "<tr>";				
-			echo "</tbody></table>";										
-			$index++;
-		}
-	} catch (Exception $e) {
-		die('Error while fetching report features');
-		DB::disconnect();
-	}
-?>
-</div>
+			?>
+		</tody>
+	</table>
 </div>
