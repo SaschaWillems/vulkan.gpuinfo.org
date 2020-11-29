b5c28ace1892746f9d054417f6ea24af8f612778
Sascha Willems <webmaster@saschawillems.de>
2020-11-29 14:02:50 +0100

    Report compare: Fetch flags and device information into class for easier access

--- report_compare.class.php
+++ report_compare.class.php
@@ -19,25 +19,87 @@
 		*
     */
 
+    class ReportCompareFlags {
+        public $has_surface_caps = false;
+        public $has_platform_details = false;
+        public $has_extended_features = false;
+        public $has_extended_properties = false;
+        public $has_vulkan_1_1_features = false;
+        public $has_vulkan_1_1_properties = false;
+        public $has_vulkan_1_2_features = false;
+        public $has_vulkan_1_2_properties = false;
+    }
+
+    class ReportDeviceInfo {
+        public $name;
+        public $driver_version;
+        public $api_version;
+        public $platform;
+        public $ostype;
+        public $reportid;
+    }
+
     class ReportCompare {
 
         private $header_column_names = ['device', 'driverversion', 'apiversion', 'os'];
         
         public $reports_ids = [];
+        public ReportCompareFlags $flags;        
+        public $device_infos = [];
 
         function __construct($reportids)
         {
             foreach($reportids as $id) {
-                if (is_int($id)) {
-                    $this->reports_ids[] = $id;
-                }
+                $this->reports_ids[] = intval($id);
             }
+            $this->flags = new ReportCompareFlags;
         }
 
         private function reportIdsParam()
         {
+            assert(count($this->reports_ids) > 0);
             return implode(",", $this->reports_ids);
         }
+
+        public function fetchData() 
+        {
+            // DB::connect();
+            // Flags for optional data
+            $this->flags->has_surface_caps = DB::getCount("SELECT count(*) from devicesurfacecapabilities where reportid in (" . $this->reportIdsParam() . ")") > 0;
+            $this->flags->has_platform_details = DB::getCount("SELECT count(*) from deviceplatformdetails where reportid in (" . $this->reportIdsParam() . ")", []) > 0;
+            $this->flags->has_extended_features = DB::getCount("SELECT count(*) from devicefeatures2 where reportid in (" . $this->reportIdsParam() . ")", []) > 0;
+            $this->flags->has_extended_properties = DB::getCount("SELECT count(*) from deviceproperties2 where reportid in (" . $this->reportIdsParam() . ")", []) > 0;
+            $this->flags->has_vulkan_1_1_features = DB::getCount("SELECT count(*) from devicefeatures11 where reportid in (" . $this->reportIdsParam() . ")", []) > 0;
+            $this->flags->has_vulkan_1_1_properties = DB::getCount("SELECT count(*) from deviceproperties11 where reportid in (" . $this->reportIdsParam() . ")", []) > 0;
+            $this->flags->has_vulkan_1_2_features = DB::getCount("SELECT count(*) from devicefeatures12 where reportid in (" . $this->reportIdsParam() . ")", []) > 0;
+            $this->flags->has_vulkan_1_2_properties = DB::getCount("SELECT count(*) from deviceproperties12 where reportid in (" . $this->reportIdsParam() . ")", []) > 0;
+            // DB::disconnect();
+            // Fetch descriptions for devices to be compared
+            try {
+                $stmnt = DB::$connection->prepare(
+                "SELECT 
+                    concat(VendorId(p.vendorid), ' ', p.devicename),
+                    p.driverversion,
+                    p.apiversion,
+                    concat(r.osname, ' ', r.osversion, ' (',  r.osarchitecture, ')'),
+                    r.ostype,
+                    r.id
+                from reports r left join deviceproperties p on (p.reportid = r.id) where r.id in (" . $this->reportIdsParam() . ")");
+                $stmnt->execute();
+            } catch (PDOException $e) {
+                die("Could not fetch report data!");
+            }
+            foreach($stmnt->fetchAll(PDO::FETCH_NUM) as $device) {
+                $device_info = new ReportDeviceInfo;
+                $device_info->name = $device[0];
+                $device_info->driver_version = $device[1];
+                $device_info->api_version = $device[2];
+                $device_info->platform = $device[3];                
+                $device_info->ostype = $device[4];
+                $device_info->reportid = $device[5];
+                $this->device_infos[] = $device_info;
+            }
+        }        
 
         public function isHeaderColumn($column_name)
         {
@@ -60,6 +122,10 @@
             echo "</th></thead><tbody>";
         }
     
+        /**
+         * Inserts the device identification rows and columns to a table (devicename, driver and api version)
+         */
+        // @todo: Deprecate
         public static function insertDeviceColumns($deviceinfo_captions, $deviceinfo_data, $count, $grouping_column = null)
         {
             for ($i = 1; $i < sizeof($deviceinfo_data[0]); ++$i) 
@@ -75,6 +141,31 @@
                 }
                 echo "</tr>";
             }
+        }
+
+        /**
+         * Insert device identification rows and columns into the current table
+         */
+        public function insertDeviceInformation($grouping_column)
+        {            
+            echo "<tr><td class='subkey'>Driver version</td>";
+            if ($grouping_column) { echo "<td>$grouping_column</td>"; }
+            foreach($this->device_infos as $device_info) {
+                echo "<td class='deviceinfo'>".$device_info->driver_version."</td>";
+            }
+            echo "</tr>";
+            echo "<tr><td class='subkey'>API version</td>";
+            if ($grouping_column) { echo "<td>$grouping_column</td>"; }
+            foreach($this->device_infos as $device_info) {
+                echo "<td class='deviceinfo'>".$device_info->api_version."</td>";
+            }
+            echo "</tr>";
+            echo "<tr><td class='subkey'>Platform</td>";
+            if ($grouping_column) { echo "<td>$grouping_column</td>"; }
+            foreach($this->device_infos as $device_info) {
+                echo "<td class='deviceinfo'>".$device_info->platform."</td>";
+            }
+            echo "</tr>";
         }
 
         public function fetchFeatures() 
