1d6c2b6dc55fc3d1d4d73ffa604c3e9d105de8a6
Sascha Willems <webmaster@saschawillems.de>
2020-12-06 14:19:48 +0100

    Added color space modes

--- functions.php
+++ functions.php
@@ -42,7 +42,7 @@ function getFormatFlags($flag)
 		0x4000 => "TRANSFER_SRC_BIT",
 		0x8000 => "TRANSFER_DST_BIT",
 	);
-	return getFlags($flags, $flag);
+	return getFlags($flags, $flag);	
 }
 
 function getImageUsageFlags($flag)
@@ -97,7 +97,7 @@ function getMemoryTypeFlags($flag)
 		0x0010 => "LAZILY_ALLOCATED_BIT",
 		0x0020 => "PROTECTED_BIT",
 		0x0040 => "DEVICE_COHERENT_BIT_AMD",
-		0x0080 => "DEVICE_UNCACHED_BIT_AMD",
+		0x0080 => "DEVICE_UNCACHED_BIT_AMD",		
 	);
 	return getFlags($flags, $flag);
 }
@@ -239,6 +239,21 @@ function getColorSpace($value)
 {
 	$modes = array(
 		"SRGB_NONLINEAR_KHR" => 0,
+		"DISPLAY_P3_NONLINEAR_EXT" => 1000104001,
+		"EXTENDED_SRGB_LINEAR_EXT" => 1000104002,
+		"DISPLAY_P3_LINEAR_EXT" => 1000104003,
+		"DCI_P3_NONLINEAR_EXT" => 1000104004,
+		"BT709_LINEAR_EXT" => 1000104005,
+		"BT709_NONLINEAR_EXT" => 1000104006,
+		"BT2020_LINEAR_EXT" => 1000104007,
+		"HDR10_ST2084_EXT" => 1000104008,
+		"DOLBYVISION_EXT" => 1000104009,
+		"HDR10_HLG_EXT" => 1000104010,
+		"ADOBERGB_LINEAR_EXT" => 1000104011,
+		"ADOBERGB_NONLINEAR_EXT" => 1000104012,
+		"PASS_THROUGH_EXT" => 1000104013,
+		"EXTENDED_SRGB_NONLINEAR_EXT" => 1000104014,
+		"DISPLAY_NATIVE_AMD" => 1000213000	
 	);
 	if (in_array($value, $modes))
 	{
