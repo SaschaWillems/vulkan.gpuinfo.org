4f6173789a12a5223fa02a713d57c0b6291ac13b
Sascha Willems <webmaster@saschawillems.de>
2020-12-09 20:39:40 +0100

    Reworked report queue family display

--- displayreport_queues.php
+++ displayreport_queues.php
@@ -19,43 +19,49 @@
 		*
 	*/
 ?>
-<table id='devicequeues' class='table table-striped table-bordered table-hover reporttable'>
+
+<?php
+$data = $report->fetchQueueFamilies();
+?>
+
+<table id='devicequeues' class='table table-striped table-bordered table-hover table-header-rotated queue-family-table'>
 	<thead>
 		<tr>
-			<td class='caption'>Property</td>
-			<td class='caption'>Value</td>
-			<td></td>
+			<th class='caption' style='border-right: 0px;'>Queue family</th>
+			<th class='caption rotate-45'><div><span>Queue count</span></div></th>
+			<th class='caption rotate-45'><div><span>timestampValidBits</span></div></th>
+			<th class='caption rotate-45'><div><span>minImageTransferGranularity.width</span></div></th>
+			<th class='caption rotate-45'><div><span>minImageTransferGranularity.height</span></div></th>
+			<th class='caption rotate-45'><div><span>minImageTransferGranularity.depth</span></div></th>
+			<th class='caption rotate-45'><div><span>Supports present</span></div></th>
+			<?php			
+			foreach($queue_flag_bits as $key => $value) {
+				echo "<th class='caption rotate-45'><div><span>$value</span></div></th>";
+			}
+			?>			
 		</tr>
 	</thead>
 	<tbody>
-	<?php		
-		$data = $report->fetchQueueFamilies();
-		$index = 0;
-		if ($data) {	
-			foreach($data as $queue_family) {
-				foreach($queue_family as $key => $value) {
-					if (in_array($key, ['id', 'reportid'])) {
-						continue;
-					}
-					echo "<tr>";
-					if ($key == 'count') {
-						$key = 'queueCount';
-					}
-					echo "<td style='width: 25%;' class='subkey'>$key</td>";
-					if ($key == 'flags') {
-						echo "<td>";
-						$flags = getQueueFlags($value);
-						listFlags($flags);
-						echo "</td>";
-					} else {
-						echo "<td>$value</td>";
-					}
-					echo "<td>Queue family $index</td>";
-					echo "</tr>";
-				}
-				$index++;				
-			}		
+		<?php
+		if ($data) {
+			foreach($data as $index => $queue_family) {
+				echo "<tr>";
+				echo "<td class='queue-family-name'>Queue family $index</td>";
+				echo "<td class='rotated-table-column'>".$queue_family["count"]."</td>";
+				echo "<td class='rotated-table-column'>".$queue_family["timestampValidBits"]."</td>";
+				echo "<td class='rotated-table-column'>".$queue_family["minImageTransferGranularity.width"]."</td>";
+				echo "<td class='rotated-table-column'>".$queue_family["minImageTransferGranularity.height"]."</td>";
+				echo "<td class='rotated-table-column'>".$queue_family["minImageTransferGranularity.depth"]."</td>";
+				echo "<td class='format-table-support'><img src='".($queue_family["supportsPresent"] ? 'icon_check.png' : 'icon_missing.png')."' width='16px'></td>";
+				foreach($queue_flag_bits as $flag_enum => $value) {
+					echo "<td class='format-table-support'>";
+					$image = ($queue_family["flags"] & $flag_enum) ? 'icon_check.png' : 'icon_missing.png';
+					echo "<img src='$image' width='16px'>";
+					echo "</td>";
+				}				
+				echo "</tr>";
+			}
 		}
-	?>
+		?>
 	</tbody>
 </table>
