8d8b7bffed46118c38711f85566f3fd63951b35d
Sascha Willems <webmaster@saschawillems.de>
2020-12-26 12:51:58 +0100

    Added api that checks if a report can be updated

---
+++ api/v3/updatereport.php
@@ -0,0 +1,30 @@
+<?php
+	/** 		
+	 *
+	 * Vulkan hardware capability database back-end
+	 *	
+	 * Copyright (C) 2020 by Sascha Willems (www.saschawillems.de)
+	 *	
+	 * This code is free software, you can redistribute it and/or
+	 * modify it under the terms of the GNU Affero General Public
+	 * License version 3 as published by the Free Software Foundation.
+	 *	
+	 * Please review the following information to ensure the GNU Lesser
+	 * General Public License version 3 requirements will be met:
+	 * http://www.gnu.org/licenses/agpl-3.0.de.html
+	 *	
+	 * The code is distributed WITHOUT ANY WARRANTY; without even the
+	 * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+	 * PURPOSE.  See the GNU AGPL 3.0 for more details.		
+	 *
+     */
+    
+    /**
+     * Implements report update logic for:
+     *  - Core 1.1 features and properties
+     *  - Core 1.2 features and properties
+     *  - Extension features and properties
+     */
+
+    // @todo: log update to reportupdatehistory
+	// @todo: overwrite json? add revision to table?
