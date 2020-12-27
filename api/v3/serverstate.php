51e7f46a69312a8261e0c8b0350eaf7c1b332de3
Sascha Willems <webmaster@saschawillems.de>
2020-12-27 11:17:59 +0100

    Serverstate api for v3

---
+++ api/v3/serverstate.php
@@ -0,0 +1,23 @@
+<?php
+	/** 		
+	 *
+	 * Vulkan hardware capability database server implementation
+	 *	
+	 * Copyright (C) 2016-2020 by Sascha Willems (www.saschawillems.de)
+	 *	
+	 * This code is free software, you can redistribute it and/or
+	 * modify it under the terms of the GNU Affero General Public
+	 * License version 3 as published by the Free Software Foundation.
+	 *	
+	 * Please review the following information to ensure the GNU Lesser
+	 * General Public License version 3 requirements will be met:
+	 * http://www.gnu.org/licenses/agpl-3.0.de.html
+	 *	
+	 * The code is distributed WITHOUT ANY WARRANTY; without even the
+ 	 * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+	 * PURPOSE.  See the GNU AGPL 3.0 for more details.		
+	 *
+	 */
+
+     echo "ok";
+?>
