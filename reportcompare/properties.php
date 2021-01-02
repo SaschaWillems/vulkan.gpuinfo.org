75f9d458b14d8ad472e872072f2a994064f2e1f5
Sascha Willems <webmaster@saschawillems.de>
2021-01-02 09:58:09 +0100

    Properties display and compare

--- reportcompare/properties.php
+++ reportcompare/properties.php
@@ -26,7 +26,6 @@ function insertCore10Properties($report_compare)
 	$report_compare->beginTab('properties_core_10', true);
 	$report_compare->beginTable('table_properties_core_10');
 	$report_compare->insertTableHeader("Property", true);
-	$report_compare->insertDeviceInformation("Device");
 
 	// Properties
 	$compare_properties = $report_compare->fetchCoreProperties('1.0');
@@ -70,11 +69,7 @@ function insertCore10Properties($report_compare)
 				break;
 			}
 		}
-
-		$row_class = "";
-		if (!$report_compare->isHeaderColumn($compare_limits->captions[$i])) {
-			$row_class = $differing_values ? "" : "class='sameCaps'";
-		};
+		$row_class = $differing_values ? "" : "class='sameCaps'";
 		echo "<tr $row_class>";
 		echo "<td class='subkey'>" . ($differing_values ? $report_compare->getDiffIcon() : "") . $compare_limits->captions[$i] . "</td>";
 		echo "<td>Limits</td>";
@@ -96,26 +91,20 @@ function insertCoreProperties($report_compare, $version)
 
 	$report_compare->beginTab('properties_core_' . str_replace('.', '', $version), $version == '1.0');
 	$report_compare->beginTable("table_properties_core_" . str_replace('.', '', $version));
-	$report_compare->insertTableHeader("Property", true);
-	$report_compare->insertDeviceInformation("Device");
+	$report_compare->insertTableHeader("Property");
 
 	for ($i = 0; $i < $compare_properties->count; $i++) {
 		// Check if row contains differing values
 		$differing_values = false;
 		for ($j = 1; $j < count($compare_properties->data); $j++) {
-			if ($compare_properties->data[$j][$i] !== $compare_properties->data[0][$i]) {
+			if (($compare_properties->data[$j][$i] !== null) && ($compare_properties->data[$j][$i] !== $compare_properties->data[0][$i])) {
 				$differing_values = true;
 				break;
 			}
 		}
-
-		$row_class = "";
-		if (!$report_compare->isHeaderColumn($compare_properties->captions[$i])) {
-			$row_class = $differing_values ? "" : "class='sameCaps'";
-		};
+		$row_class = $differing_values ? "" : "class='sameCaps'";
 		echo "<tr $row_class>";
 		echo "<td class='subkey'>" . ($differing_values ? $report_compare->getDiffIcon() : "") . $compare_properties->captions[$i] . "</td>";
-		echo "<td>Property</td>";
 		for ($j = 0; $j < count($compare_properties->data); $j++) {
 			$value = $compare_properties->data[$j][$i];
 			$displayvalue = (($value !== null) ? getPropertyDisplayValue($compare_properties->captions[$i], $value) : "<span class='na'>n/a</span>");
@@ -136,33 +125,35 @@ function insertExtensionProperties($report_compare)
 	$report_compare->insertDeviceInformation("Device");
 
 	$extended_properties = [];
-	$extended_properties_reports = [];
-	if ($report_compare->fetchExtensionProperties($extended_properties, $extended_properties_reports)) {
+	$reports = [];
+	if ($report_compare->fetchExtensionProperties($extended_properties, $reports)) {
 		foreach ($extended_properties as $extension => $features) {
 			foreach ($features as $feature) {
 				$html = '';
 				$diff = false;
 				$last_val = null;
-				foreach ($extended_properties_reports as $index => $extended_properties_report) {
-					$ext_present = array_key_exists($extension, $extended_properties_report);
+				foreach ($reports as $index => $report) {
+					$ext_present = array_key_exists($extension, $report);
 					if ($ext_present) {
-						$supported = false;
-						if (array_key_exists($extension, $extended_properties_report)) {
-							foreach ($extended_properties_report[$extension] as $ext_report_f) {
-								if ($ext_report_f['name'] == $feature['name']) {
-									$supported = $ext_report_f['value'];
+						$curr_val = null;
+						if (array_key_exists($extension, $report)) {
+							foreach ($report[$extension] as $ext_report_property) {
+								if ($ext_report_property['name'] == $feature['name']) {
+									$curr_val = $ext_report_property['value'];
+									break;
 								}
 							}
 						}
-						if ($index > 0 && $supported != $last_val) {
+						if (($index > 0) && ($curr_val !== null) && ($curr_val !== $last_val)) {
 							$diff = true;
 						}
-						$last_val = $supported;
-						$displayvalue = getPropertyDisplayValue($feature['name'], $supported);
+						$last_val = $curr_val;
+					};
+					if ($curr_val !== null) {
+						$displayvalue = getPropertyDisplayValue($feature['name'], $curr_val);
 						$html .= "<td>$displayvalue</td>";
 					} else {
 						$html .= "<td class='na'>n.a.</td>";
-						$diff = true;
 					}
 				}
 				$html = "<tr class='" . ($diff ? "diff" : "same") . "'><td class='subkey'>" . ($diff ? $report_compare->getDiffIcon() : "") . $feature['name'] . "</td><td>" . $extension . "</td>" . $html . "</tr>";
