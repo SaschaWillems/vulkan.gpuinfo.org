4ea60fee8c377f9416ac498e293806d1098b7fbb
Sascha Willems <webmaster@saschawillems.de>
2021-01-05 21:57:14 +0100

    Extension property and features listings can be pre-filtered via url parameter

--- list_features_extensions.php
+++ list_features_extensions.php
@@ -1,5 +1,5 @@
-<?php 
-	/* 		
+<?php
+/* 		
 		*
 		* Vulkan hardware capability database server implementation
 		*	
@@ -18,29 +18,40 @@
 		* PURPOSE.  See the GNU AGPL 3.0 for more details.		
 		*
 	*/
-	
-	include 'page_generator.php';
-	include './database/database.class.php';
-	include './includes/functions.php';	
-	
-	$platform = "windows";
-	if (isset($_GET['platform'])) {
-		$platform = $_GET['platform'];
-	}
 
-	PageGenerator::header("Extension features listing");
+include 'page_generator.php';
+include './database/database.class.php';
+include './includes/functions.php';
+
+$platform = "windows";
+if (isset($_GET['platform'])) {
+	$platform = $_GET['platform'];
+}
+
+$search = null;
+if (isset($_GET['search'])) {
+	$search = $_GET['search'];
+}
+
+PageGenerator::header("Extension features listing");
 ?>
-	
+
 <div class='header'>
-	<?php echo "<h4>Extension device feature coverage for <img src='images/".$platform."logo.png' height='14px' style='padding-right:5px'/>".ucfirst($platform); ?>	
-</div>			
+	<?php echo "<h4>Extension device feature coverage for <img src='images/" . $platform . "logo.png' height='14px' style='padding-right:5px'/>" . ucfirst($platform); ?>
+</div>
 
 <center>
 	<div>
 		<ul class='nav nav-tabs'>
-			<li <?php if ($platform == "windows") { echo "class='active'"; } ?>> <a href='list_features_extensions.php?platform=windows'><img src="images/windowslogo.png" height="14px" style="padding-right:5px">Windows</a> </li>
-			<li <?php if ($platform == "linux")   { echo "class='active'"; } ?>> <a href='list_features_extensions.php?platform=linux'><img src="images/linuxlogo.png" height="16px" style="padding-right:4px">Linux</a> </li>
-			<li <?php if ($platform == "android") { echo "class='active'"; } ?>> <a href='list_features_extensions.php?platform=android'><img src="images/androidlogo.png" height="16px" style="padding-right:4px">Android</a> </li>
+			<li <?php if ($platform == "windows") {
+					echo "class='active'";
+				} ?>> <a href='list_features_extensions.php?platform=windows'><img src="images/windowslogo.png" height="14px" style="padding-right:5px">Windows</a> </li>
+			<li <?php if ($platform == "linux") {
+					echo "class='active'";
+				} ?>> <a href='list_features_extensions.php?platform=linux'><img src="images/linuxlogo.png" height="16px" style="padding-right:4px">Linux</a> </li>
+			<li <?php if ($platform == "android") {
+					echo "class='active'";
+				} ?>> <a href='list_features_extensions.php?platform=android'><img src="images/androidlogo.png" height="16px" style="padding-right:4px">Android</a> </li>
 		</ul>
 	</div>
 
@@ -49,24 +60,24 @@
 
 		<table id="features" class="table table-striped table-bordered table-hover responsive" style='width:auto;'>
 			<thead>
-				<tr>			
+				<tr>
 					<th></th>
 					<th>Feature</th>
 					<th style="text-align: center;"><img src='images/icons/check.png' width=16px></th>
 					<th style="text-align: center;"><img src='images/icons/missing.png' width=16px></th>
-				</tr>			
+				</tr>
 			</thead>
-			<tbody>		
-				<?php	
-					DB::connect();
-					try {
-						$os_type = ostype($platform);
-						// Get the total count of devices that have been submitted with a report version that has support for extension features (introduced with 1.4)
-						$stmnt =DB::$connection->prepare("SELECT COUNT(DISTINCT IFNULL(r.displayname, dp.devicename)) FROM reports r JOIN deviceproperties dp ON r.id = dp.reportid WHERE r.ostype = :ostype AND r.version >= '1.4'");
-						$stmnt->execute(['ostype' => $os_type]);
-						$device_count = $stmnt->fetchColumn();
-	
-						$stmnt = DB::$connection->prepare('SELECT 
+			<tbody>
+				<?php
+				DB::connect();
+				try {
+					$os_type = ostype($platform);
+					// Get the total count of devices that have been submitted with a report version that has support for extension features (introduced with 1.4)
+					$stmnt = DB::$connection->prepare("SELECT COUNT(DISTINCT IFNULL(r.displayname, dp.devicename)) FROM reports r JOIN deviceproperties dp ON r.id = dp.reportid WHERE r.ostype = :ostype AND r.version >= '1.4'");
+					$stmnt->execute(['ostype' => $os_type]);
+					$device_count = $stmnt->fetchColumn();
+
+					$stmnt = DB::$connection->prepare('SELECT 
 								extension,
 								name,
 								COUNT(DISTINCT IFNULL(r.displayname, dp.devicename)) AS supporteddevices
@@ -80,64 +91,76 @@
 								supported = 1 AND r.ostype = :ostype
 							GROUP BY extension , name
 							ORDER BY extension ASC , name ASC');
-						$stmnt->execute(['ostype' => $os_type]);
-	
-						if ($stmnt->rowCount() > 0) { 
-							while ($feature = $stmnt->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT)) {
-								$coverageLink = "listdevicescoverage.php?extensionname=".$feature['extension']."&extensionfeature=".$feature['name']."&platform=$platform";
-								$coverage = round($feature['supporteddevices'] / $device_count * 100, 1);
-								echo "<tr>";
-								echo "<td>".$feature['extension']."</td>";
-								echo "<td class='subkey'>".$feature['name']."</td>";
-								echo "<td class='text-center'><a class='supported' href=\"$coverageLink\">$coverage<span style='font-size:10px;'>%</span></a></td>";
-								echo "<td class='text-center'><a class='na' href=\"$coverageLink&option=not\">".round(100 - $coverage, 1)."<span style='font-size:10px;'>%</span></a></td>";
-								echo "</tr>";
-							}
+					$stmnt->execute(['ostype' => $os_type]);
+
+					if ($stmnt->rowCount() > 0) {
+						while ($feature = $stmnt->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT)) {
+							$coverageLink = "listdevicescoverage.php?extensionname=" . $feature['extension'] . "&extensionfeature=" . $feature['name'] . "&platform=$platform";
+							$coverage = round($feature['supporteddevices'] / $device_count * 100, 1);
+							echo "<tr>";
+							echo "<td>" . $feature['extension'] . "</td>";
+							echo "<td class='subkey'>" . $feature['name'] . "</td>";
+							echo "<td class='text-center'><a class='supported' href=\"$coverageLink\">$coverage<span style='font-size:10px;'>%</span></a></td>";
+							echo "<td class='text-center'><a class='na' href=\"$coverageLink&option=not\">" . round(100 - $coverage, 1) . "<span style='font-size:10px;'>%</span></a></td>";
+							echo "</tr>";
 						}
-	
-					} catch (PDOException $e) {
-						echo "<b>Error while fetching data!</b><br>";
-					}					
-					DB::disconnect();			
-				?>   
+					}
+				} catch (PDOException $e) {
+					echo "<b>Error while fetching data!</b><br>";
+				}
+				DB::disconnect();
+				?>
 			</tbody>
-		</table>  
+		</table>
 
 	</div>
 
-<script>
-	$(document).ready(function() {
-		var table = $('#features').DataTable({
-			"pageLength" : -1,
-			"paging" : false,
-			"order": [], 
-			"columnDefs": [
-				{ "visible": false, "targets": 0 }
-			],				
-			"searchHighlight": true,
-			"bAutoWidth": false,
-			"sDom": 'flpt',
-			"deferRender": true,
-			"processing": true,
-			"drawCallback": function (settings) {
-				var api = this.api();
-				var rows = api.rows( {page:'current'} ).nodes();
-				var last = null;
-				api.column(0, {page:'current'} ).data().each( function ( group, i ) {
-					if ( last !== group ) {
-						$(rows).eq( i ).before(
-							'<tr><td colspan="3" class="group">'+group+'</td></tr>'
-						);
-						last = group;
-					}
-				});
+	<script>
+		$(document).ready(function() {
+			var table = $('#features').DataTable({
+				"pageLength": -1,
+				"paging": false,
+				"order": [],
+				"columnDefs": [{
+					"visible": false,
+					"targets": 0
+				}],
+				"searchHighlight": true,
+				"bAutoWidth": false,
+				"sDom": 'flpt',
+				"deferRender": true,
+				"processing": true,
+				"drawCallback": function(settings) {
+					var api = this.api();
+					var rows = api.rows({
+						page: 'current'
+					}).nodes();
+					var last = null;
+					api.column(0, {
+						page: 'current'
+					}).data().each(function(group, i) {
+						if (last !== group) {
+							$(rows).eq(i).before(
+								'<tr><td colspan="3" class="group">' + group + '</td></tr>'
+							);
+							last = group;
+						}
+					});
+				}
+			});
+			<?php
+			if ($search !== null) {
+			?>
+				table.search('\\b<?= $search ?>\\b', true, false).draw();
+			<?php
 			}
+			?>
 		});
-	});	
-</script>
+	</script>
 
-<?php PageGenerator::footer(); ?>
-	
+	<?php PageGenerator::footer(); ?>
+
 </center>
 </body>
+
 </html>
--- list_properties_extensions.php
+++ list_properties_extensions.php
@@ -1,73 +1,85 @@
-<?php 
-	/* 		
-		*
-		* Vulkan hardware capability database server implementation
-		*	
-		* Copyright (C) 2016-2020 Sascha Willems (www.saschawillems.de)
-		*	
-		* This code is free software, you can redistribute it and/or
-		* modify it under the terms of the GNU Affero General Public
-		* License version 3 as published by the Free Software Foundation.
-		*	
-		* Please review the following information to ensure the GNU Lesser
-		* General Public License version 3 requirements will be met:
-		* http://www.gnu.org/licenses/agpl-3.0.de.html
-		*	
-		* The code is distributed WITHOUT ANY WARRANTY; without even the
-		* implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-		* PURPOSE.  See the GNU AGPL 3.0 for more details.		
-		*
-	*/
-	
-	include 'page_generator.php';
-	include './database/database.class.php';
-	include './includes/functions.php';	
-	
-	$platform = "windows";
-	if (isset($_GET['platform'])) {
-		$platform = $_GET['platform'];
-	}
+<?php
 
-	PageGenerator::header("Extension properties listing");
+/**
+ *
+ * Vulkan hardware capability database server implementation
+ *	
+ * Copyright (C) 2016-2021 Sascha Willems (www.saschawillems.de)
+ *	
+ * This code is free software, you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public
+ * License version 3 as published by the Free Software Foundation.
+ *	
+ * Please review the following information to ensure the GNU Lesser
+ * General Public License version 3 requirements will be met:
+ * http://www.gnu.org/licenses/agpl-3.0.de.html
+ *	
+ * The code is distributed WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU AGPL 3.0 for more details.		
+ *
+ */
+
+require 'page_generator.php';
+require './database/database.class.php';
+require './includes/functions.php';
+
+$platform = "windows";
+if (isset($_GET['platform'])) {
+	$platform = $_GET['platform'];
+}
+
+$search = null;
+if (isset($_GET['search'])) {
+	$search = $_GET['search'];
+}
+
+PageGenerator::header("Extension properties listing");
 ?>
-	
+
 <div class='header'>
-	<?php echo "<h4>Extension device properties for <img src='images/".$platform."logo.png' height='14px' style='padding-right:5px'/>".ucfirst($platform); ?>	
-</div>			
+	<?php echo "<h4>Extension device properties for <img src='images/" . $platform . "logo.png' height='14px' style='padding-right:5px'/>" . ucfirst($platform); ?>
+</div>
 
 <center>
 	<div>
 		<ul class='nav nav-tabs'>
-			<li <?php if ($platform == "windows") { echo "class='active'"; } ?>> <a href='list_properties_extensions.php?platform=windows'><img src="images/windowslogo.png" height="14px" style="padding-right:5px">Windows</a> </li>
-			<li <?php if ($platform == "linux")   { echo "class='active'"; } ?>> <a href='list_properties_extensions.php?platform=linux'><img src="images/linuxlogo.png" height="16px" style="padding-right:4px">Linux</a> </li>
-			<li <?php if ($platform == "android") { echo "class='active'"; } ?>> <a href='list_properties_extensions.php?platform=android'><img src="images/androidlogo.png" height="16px" style="padding-right:4px">Android</a> </li>
+			<li <?php if ($platform == "windows") {
+					echo "class='active'";
+				} ?>> <a href='list_properties_extensions.php?platform=windows'><img src="images/windowslogo.png" height="14px" style="padding-right:5px">Windows</a> </li>
+			<li <?php if ($platform == "linux") {
+					echo "class='active'";
+				} ?>> <a href='list_properties_extensions.php?platform=linux'><img src="images/linuxlogo.png" height="16px" style="padding-right:4px">Linux</a> </li>
+			<li <?php if ($platform == "android") {
+					echo "class='active'";
+				} ?>> <a href='list_properties_extensions.php?platform=android'><img src="images/androidlogo.png" height="16px" style="padding-right:4px">Android</a> </li>
 		</ul>
 	</div>
 
 
 	<div class='tablediv' style='width:auto; display: inline-block;'>
 
-		<table id="features" class="table table-striped table-bordered table-hover responsive" style='width:auto;'>
+		<table id="properties" class="table table-striped table-bordered table-hover responsive" style='width:auto;'>
 			<thead>
-				<tr>			
+				<tr>
 					<th></th>
 					<th>Property</th>
 					<th style="text-align: center;">Type</th>
 					<th style="text-align: center;"></th>
-				</tr>			
+				</tr>
 			</thead>
-			<tbody>		
-				<?php	
-					DB::connect();
-					try {
-						$os_type = ostype($platform);
-						// Get the total count of devices that have been submitted with a report version that has support for extension features (introduced with 1.4)
-						$stmnt =DB::$connection->prepare("SELECT COUNT(DISTINCT IFNULL(r.displayname, dp.devicename)) FROM reports r JOIN deviceproperties dp ON r.id = dp.reportid WHERE r.ostype = :ostype AND r.version >= '1.4'");
-						$stmnt->execute(['ostype' => $os_type]);
-						$device_count = $stmnt->fetchColumn();
-	
-						$stmnt = DB::$connection->prepare(
-							"SELECT extension, name, type, sum(supporteddevices) as supporteddevices FROM
+			<tbody>
+				<?php
+				DB::connect();
+				try {
+					$os_type = ostype($platform);
+					// Get the total count of devices that have been submitted with a report version that has support for extension features (introduced with 1.4)
+					$stmnt = DB::$connection->prepare("SELECT COUNT(DISTINCT IFNULL(r.displayname, dp.devicename)) FROM reports r JOIN deviceproperties dp ON r.id = dp.reportid WHERE r.ostype = :ostype AND r.version >= '1.4'");
+					$stmnt->execute(['ostype' => $os_type]);
+					$device_count = $stmnt->fetchColumn();
+
+					$stmnt = DB::$connection->prepare(
+						"SELECT extension, name, type, sum(supporteddevices) as supporteddevices FROM
 							(
 							SELECT 
 								extension,
@@ -120,70 +132,83 @@
 							GROUP BY extension , name
 							) tbl
 							GROUP BY extension, name, type
-							ORDER BY extension ASC , name ASC");
-						$stmnt->execute(['ostype' => $os_type]);
-	
-						if ($stmnt->rowCount() > 0) { 
-							while ($property = $stmnt->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT)) {
-								echo "<tr>";
-								echo "<td>".$property['extension']."</td>";
-								echo "<td class='subkey'>".$property['name']."</td>";
-								echo "<td class='text-center'>".ucfirst($property['type'])."</td>";
-								if ($property['type'] == 'coverage') {
-									$coverageLink = "listdevicescoverage.php?extensionname=".$property['extension']."&extensionproperty=".$property['name']."&platform=$platform";
-									$coverage = round($property['supporteddevices'] / $device_count * 100, 1);
-									echo "<td class='text-center'><a class='supported' href=\"$coverageLink\">$coverage<span style='font-size:10px;'>%</span></a></td>";
-								} else {
-									$link = "<a href='displayextensionproperty.php?name=".$property['name']."&platform=$platform'>";
-									echo "<td class='text-center'>".$link."Listing</a></td>";
-								}
-								echo "</tr>";
+							ORDER BY extension ASC , name ASC"
+					);
+					$stmnt->execute(['ostype' => $os_type]);
+
+					if ($stmnt->rowCount() > 0) {
+						while ($property = $stmnt->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT)) {
+							echo "<tr>";
+							echo "<td>" . $property['extension'] . "</td>";
+							echo "<td class='subkey'>" . $property['name'] . "</td>";
+							echo "<td class='text-center'>" . ucfirst($property['type']) . "</td>";
+							if ($property['type'] == 'coverage') {
+								$coverageLink = "listdevicescoverage.php?extensionname=" . $property['extension'] . "&extensionproperty=" . $property['name'] . "&platform=$platform";
+								$coverage = round($property['supporteddevices'] / $device_count * 100, 1);
+								echo "<td class='text-center'><a class='supported' href=\"$coverageLink\">$coverage<span style='font-size:10px;'>%</span></a></td>";
+							} else {
+								$link = "<a href='displayextensionproperty.php?name=" . $property['name'] . "&platform=$platform'>";
+								echo "<td class='text-center'>" . $link . "Listing</a></td>";
 							}
+							echo "</tr>";
 						}
-	
-					} catch (PDOException $e) {
-						echo "<b>Error while fetching data!</b><br>";
-					}					
-					DB::disconnect();			
-				?>   
+					}
+				} catch (PDOException $e) {
+					echo "<b>Error while fetching data!</b><br>";
+				}
+				DB::disconnect();
+				?>
 			</tbody>
-		</table>  
+		</table>
 
 	</div>
 
-<script>
-	$(document).ready(function() {
-		var table = $('#features').DataTable({
-			"pageLength" : -1,
-			"paging" : false,
-			"order": [], 
-			"columnDefs": [
-				{ "visible": false, "targets": 0 }
-			],				
-			"searchHighlight": true,
-			"bAutoWidth": false,
-			"sDom": 'flpt',
-			"deferRender": true,
-			"processing": true,
-			"drawCallback": function (settings) {
-				var api = this.api();
-				var rows = api.rows( {page:'current'} ).nodes();
-				var last = null;
-				api.column(0, {page:'current'} ).data().each( function ( group, i ) {
-					if ( last !== group ) {
-						$(rows).eq( i ).before(
-							'<tr><td colspan="3" class="group">'+group+'</td></tr>'
-						);
-						last = group;
-					}
-				});
+	<script>
+		$(document).ready(function() {
+			var table = $('#properties').DataTable({
+				"pageLength": -1,
+				"paging": false,
+				"order": [],
+				"columnDefs": [{
+					"visible": false,
+					"targets": 0
+				}],
+				"searchHighlight": true,
+				"bAutoWidth": false,
+				"sDom": 'flpt',
+				"deferRender": true,
+				"processing": true,
+				"drawCallback": function(settings) {
+					var api = this.api();
+					var rows = api.rows({
+						page: 'current'
+					}).nodes();
+					var last = null;
+					api.column(0, {
+						page: 'current'
+					}).data().each(function(group, i) {
+						if (last !== group) {
+							$(rows).eq(i).before(
+								'<tr><td colspan="3" class="group">' + group + '</td></tr>'
+							);
+							last = group;
+						}
+					});
+				}
+			});
+			<?php
+			if ($search !== null) {
+			?>
+				table.search('\\b<?=$search?>\\b', true, false).draw();
+			<?php
 			}
+			?>
 		});
-	});	
-</script>
+	</script>
 
-<?php PageGenerator::footer(); ?>
-	
+	<?php PageGenerator::footer(); ?>
+
 </center>
 </body>
+
 </html>
