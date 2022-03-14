d968e6183b0e4a74955f8af6fba64c4401522a7b
Sascha Willems <webmaster@saschawillems.de>
2022-03-14 20:17:27 +0100

    Added api level filter info to core feature listings

--- database/sqlrepository.php
+++ database/sqlrepository.php
@@ -23,6 +23,11 @@
 /** Builder class for the SQL statements used on different pages */
 class SqlRepository {
 
+    const VK_API_VERSION_1_0 = '1.0';
+    const VK_API_VERSION_1_1 = '1.1';
+    const VK_API_VERSION_1_2 = '1.2';
+    const VK_API_VERSION_1_3 = '1.3';
+
     public static function getMinApiVersion() {
         if (isset($_SESSION['minversion'])) {
             return $_SESSION['minversion'];
@@ -45,8 +50,8 @@ class SqlRepository {
         }
     }
 
-    public static function deviceCount() {
-        $sql = "SELECT count(DISTINCT displayname) from reports r";
+    public static function deviceCount($join = null) {
+        $sql = "SELECT count(DISTINCT displayname) from reports r $join";
         $ostype = self::getOSType();
         if ($ostype !== null) {
             self::appendCondition($sql, "r.ostype = :ostype");
@@ -65,7 +70,7 @@ class SqlRepository {
 
     /** Global extension listing */
     public static function listExtensions() {
-        $params     = [];
+        $params = [];
         $sql ="SELECT e.name, count(distinct displayname) as coverage from extensions e 
             join deviceextensions de on de.extensionid = e.id 
             join reports r on r.id = de.reportid";
@@ -83,6 +88,75 @@ class SqlRepository {
         $stmnt = DB::$connection->prepare($sql);
         $stmnt->execute($params);
         return $stmnt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /** Global core property listings */
+    public static function listCoreFeatures($version) { 
+        $table = 'devicefeatures';
+        switch ($version) {
+            case self::VK_API_VERSION_1_1:
+                $table = 'devicefeatures11';
+                break;
+            case self::VK_API_VERSION_1_2:
+                $table = 'devicefeatures12';
+                break;
+            case self::VK_API_VERSION_1_3:
+                $table = 'devicefeatures13';
+                break;
+        }
+
+        // Collect feature column names
+        $sql = "SELECT COLUMN_NAME from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = '$table' and COLUMN_NAME not in ('reportid')";
+        $stmnt = DB::$connection->prepare($sql);
+        $stmnt->execute();
+        $features = [];
+        $sqlColumns = "";
+        while ($row = $stmnt->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
+            $sqlColumns .= "max(" . $row[0] . ") as $row[0],";
+        }
+        $sqlColumnList = substr($sqlColumns, 0, -1);
+
+        // Get total count for coverage calculation
+        $join = null;
+        if ($version !== self::VK_API_VERSION_1_0) {
+            $join = "join $table df on df.reportid = id";
+        }
+        $deviceCount = SqlRepository::deviceCount($join);
+
+        // Get device support coverage
+        $params = [];
+        $sql ="SELECT ifnull(r.displayname, dp.devicename) as device, $sqlColumnList FROM $table df join deviceproperties dp on dp.reportid = df.reportid join reports r on r.id = df.reportid";
+        $ostype = self::getOSType();
+        if ($ostype !== null) {            
+            self::appendCondition($sql, "ostype = :ostype");
+            $params['ostype'] = $ostype;
+        }
+        $apiversion = self::getMinApiVersion();
+        if ($apiversion) {
+            self::appendCondition($sql, "r.apiversion >= :apiversion");
+            $params['apiversion'] = $apiversion;
+        }
+        $sql .= " group by device";
+
+        // $supportedCounts = [];
+        $stmnt = DB::$connection->prepare($sql);
+        $stmnt->execute($params);
+        while ($row = $stmnt->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT)) {
+            foreach ($row as $key => $value) {
+                if (strcasecmp($key, 'device') != 0) {
+                    $features[$key] += $value;
+                }
+            }
+        }
+
+        foreach($features as $feature => &$coverage) {
+            if ($deviceCount > 0) {
+                $coverage = round($coverage / $deviceCount * 100, 1);
+            } else {
+                $coverage = 0;
+            }
+        }
+        return $features;
     }
 
 }
--- listfeaturescore12.php
+++ listfeaturescore12.php
@@ -4,7 +4,7 @@
  *
  * Vulkan hardware capability database server implementation
  *	
- * Copyright (C) 2016-2020 Sascha Willems (www.saschawillems.de)
+ * Copyright (C) 2016-2022 Sascha Willems (www.saschawillems.de)
  *	
  * This code is free software, you can redistribute it and/or
  * modify it under the terms of the GNU Affero General Public
@@ -22,6 +22,7 @@
 
 require 'pagegenerator.php';
 require './database/database.class.php';
+require './database/sqlrepository.php';
 require './includes/functions.php';
 require './includes/constants.php';
 
@@ -34,7 +35,7 @@ PageGenerator::header("Core 1.2 features");
 ?>
 
 <div class='header'>
-	<?php echo "<h4>Core 1.2 feature coverage on ".PageGenerator::platformInfo($platform) ?>
+	<?php echo "<h4>Core 1.2 feature coverage on ".PageGenerator::filterInfo($platform) ?>
 </div>
 <div class="alert alert-info" role="alert" style="text-align: center">
 	<b>Note:</b> Data is based on reports submitted or updated with version 3.0 or newer of the Hardware Capability Viewer and does not contain reports from earlier versions.
@@ -59,48 +60,14 @@ PageGenerator::header("Core 1.2 features");
 				<?php
 				DB::connect();
 				try {
-					$os_filter = null;
-					$params = [];
-					if ($platform !== 'all') {
-						$params['ostype'] = ostype($platform);
-						$os_filter = 'WHERE r.ostype = :ostype';
-					}					
-					$deviceCount = DB::getCount("SELECT count(distinct(displayname)) from reports r join devicefeatures12 dp on dp.reportid = id $os_filter", $params);					
-					if ($deviceCount > 0) {
-						// Collect feature column names
-						$sql = "SELECT COLUMN_NAME from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'devicefeatures12' and COLUMN_NAME not in ('reportid')";
-						$stmnt = DB::$connection->prepare($sql);
-						$stmnt->execute();
-
-						$features = [];
-						$columns = [];
-						while ($row = $stmnt->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
-							$features[] = $row[0];
-							$columns[] = "max(" . $row[0] . ") as $row[0]";
-						}
-
-						$supportedCounts = [];
-						$stmnt = DB::$connection->prepare(
-							"SELECT r.displayname as device, " . implode(',', $columns) . " FROM devicefeatures12 df join deviceproperties dp on dp.reportid = df.reportid join reports r on r.id = df.reportid $os_filter group by device"
-						);
-						$stmnt->execute($params);
-						while ($row = $stmnt->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT)) {
-							foreach ($row as $key => $value) {
-								if (strcasecmp($key, 'device') != 0) {
-									$supportedCounts[$key] += $value;
-								}
-							}
-						}
-
-						foreach ($features as $feature) {
-							$coverageLink = "listdevicescoverage.php?core=1.2&feature=" . $feature . "&platform=$platform";
-							$coverage = $deviceCount > 0 ? round($supportedCounts[$feature] / $deviceCount * 100, 1) : 0;
-							echo "<tr>";
-							echo "<td>" . $feature . "</td>";
-							echo "<td class='text-center'><a class='supported' href=\"$coverageLink\">$coverage<span style='font-size:10px;'>%</span></a></td>";
-							echo "<td class='text-center'><a class='na' href=\"$coverageLink&option=not\">" . round(100 - $coverage, 1) . "<span style='font-size:10px;'>%</span></a></td>";
-							echo "</tr>";
-						}
+					$features = SqlRepository::listCoreFeatures(SqlRepository::VK_API_VERSION_1_2);
+					foreach ($features as $feature => $coverage) {
+						$coverageLink = "listdevicescoverage.php?core=1.2&feature=$feature&platform=$platform";
+						echo "<tr>";
+						echo "<td>" . $feature . "</td>";
+						echo "<td class='text-center'><a class='supported' href=\"$coverageLink\">$coverage<span style='font-size:10px;'>%</span></a></td>";
+						echo "<td class='text-center'><a class='na' href=\"$coverageLink&option=not\">" . round(100 - $coverage, 1) . "<span style='font-size:10px;'>%</span></a></td>";
+						echo "</tr>";
 					}
 				} catch (PDOException $e) {
 					echo "<b>Error while fetching data!</b><br>";
