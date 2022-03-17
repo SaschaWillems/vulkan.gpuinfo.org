4675e20740d63b9eaba062be7d9304384f21b3b2
Sascha Willems <webmaster@saschawillems.de>
2022-03-17 16:13:50 +0100

    Added api level filter info to core property listing
    Except 1.0

--- database/sqlrepository.php
+++ database/sqlrepository.php
@@ -103,7 +103,7 @@ class SqlRepository {
         return $stmnt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    /** Global core property listings */
+    /** Global core feature listings */
     public static function listCoreFeatures($version) { 
         $table = 'devicefeatures';
         switch ($version) {
@@ -209,6 +209,158 @@ class SqlRepository {
             ];
         }
         return $features;
+    }
+
+    /** Global core property listings */
+    public static function listCoreProperties($version) { 
+        $table = 'deviceproperties';
+        switch ($version) {
+            case self::VK_API_VERSION_1_1:
+                $table = 'deviceproperties11';
+                break;
+            case self::VK_API_VERSION_1_2:
+                $table = 'deviceproperties12';
+                break;
+            case self::VK_API_VERSION_1_3:
+                $table = 'deviceproperties13';
+                break;
+        }
+
+        // Columns with coverage numbers
+        $coverage_columns = [
+            // VK 1.0
+            'residencyAlignedMipSize',
+            'residencyNonResidentStrict',
+            'residencyStandard2DBlockShape',
+            'residencyStandard2DMultisampleBlockShape',
+            'residencyStandard3DBlockShape',
+            'subgroupProperties.quadOperationsInAllStages',
+            // VK 1.1
+            'deviceLUIDValid',
+            'subgroupQuadOperationsInAllStages',
+            'protectedNoFault',            
+            // VK 1.2
+            'shaderSignedZeroInfNanPreserveFloat16',
+            'shaderSignedZeroInfNanPreserveFloat32',
+            'shaderSignedZeroInfNanPreserveFloat64',
+            'shaderDenormPreserveFloat16',
+            'shaderDenormPreserveFloat32',
+            'shaderDenormPreserveFloat64',
+            'shaderDenormFlushToZeroFloat16',
+            'shaderDenormFlushToZeroFloat32',
+            'shaderDenormFlushToZeroFloat64',
+            'shaderRoundingModeRTEFloat16',
+            'shaderRoundingModeRTEFloat32',
+            'shaderRoundingModeRTEFloat64',
+            'shaderRoundingModeRTZFloat16',
+            'shaderRoundingModeRTZFloat32',
+            'shaderRoundingModeRTZFloat64',
+            'shaderUniformBufferArrayNonUniformIndexingNative',
+            'shaderSampledImageArrayNonUniformIndexingNative',
+            'shaderStorageBufferArrayNonUniformIndexingNative',
+            'shaderStorageImageArrayNonUniformIndexingNative',
+            'shaderInputAttachmentArrayNonUniformIndexingNative',
+            'robustBufferAccessUpdateAfterBind',
+            'quadDivergentImplicitLod',
+            'independentResolveNone',
+            'independentResolve',
+            'filterMinmaxSingleComponentFormats',
+            'filterMinmaxImageComponentMapping',            
+            // VK 1.3
+            'idp8BitUnsignedAccelerated',
+            'idp8BitSignedAccelerated',
+            'idp8BitMixedSignednessAccelerated',
+            'idp4x8BitPackedUnsignedAccelerated',
+            'idp4x8BitPackedSignedAccelerated',
+            'idp4x8BitPackedMixedSignednessAccelerated',
+            'idp16BitUnsignedAccelerated',
+            'idp16BitSignedAccelerated',
+            'idp16BitMixedSignednessAccelerated',
+            'idp32BitUnsignedAccelerated',
+            'idp32BitSignedAccelerated',
+            'idp32BitMixedSignednessAccelerated',
+            'idp64BitUnsignedAccelerated',
+            'idp64BitSignedAccelerated',
+            'idp64BitMixedSignednessAccelerated',
+            'idpAccumulatingSaturating8BitUnsignedAccelerated',
+            'idpAccumulatingSaturating8BitSignedAccelerated',
+            'idpAccumulatingSaturating8BitMixedSignednessAccelerated',
+            'idpAccumulatingSaturating4x8BitPackedUnsignedAccelerated',
+            'idpAccumulatingSaturating4x8BitPackedSignedAccelerated',
+            'idpAccumulatingSaturating4x8BitPackedMixedSignednessAccelerated',
+            'idpAccumulatingSaturating16BitUnsignedAccelerated',
+            'idpAccumulatingSaturating16BitSignedAccelerated',
+            'idpAccumulatingSaturating16BitMixedSignednessAccelerated',
+            'idpAccumulatingSaturating32BitUnsignedAccelerated',
+            'idpAccumulatingSaturating32BitSignedAccelerated',
+            'idpAccumulatingSaturating32BitMixedSignednessAccelerated',
+            'idpAccumulatingSaturating64BitUnsignedAccelerated',
+            'idpAccumulatingSaturating64BitSignedAccelerated',
+            'idpAccumulatingSaturating64BitMixedSignednessAccelerated',
+            'storageTexelBufferOffsetSingleTexelAlignment',
+            'uniformTexelBufferOffsetSingleTexelAlignment',            
+        ];
+       
+        // Columns to ignore (not part of the api structure)
+        $ignore_columns = [
+            'reportid',
+            'headerversion',
+            'driverversionraw',
+            'pipelineCacheUUID',
+            'apiversionraw',
+            'productManufacturer',
+            'productModel'
+        ];
+
+        // Get total count for coverage calculation
+        $join = null;
+        if ($version !== self::VK_API_VERSION_1_0) {
+            $join = "join $table df on df.reportid = id";
+        }
+        $deviceCount = SqlRepository::deviceCount($join);        
+
+        // Collect property column names
+        $sql = "SELECT COLUMN_NAME from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = '$table' and COLUMN_NAME not in ("."'" . implode("','", $ignore_columns) . "') order by COLUMN_NAME";
+        $stmnt = DB::$connection->prepare($sql);
+        $stmnt->execute();
+        $properties = [];
+        $sqlColumns = "";
+        while ($row = $stmnt->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
+            if (in_array($row[0], $coverage_columns)) {
+                // Device coverage with numbers
+                $sqlColumns .= "max(" . $row[0] . ") as $row[0],";
+            } else {
+                // Value listing/limit (no numbers)
+                $sqlColumns .= "null as $row[0],";
+            }
+        }
+        $sqlColumnList = rtrim($sqlColumns, ',');
+        $sql = "SELECT r.displayname as device, $sqlColumnList FROM $table dp join reports r on r.id = dp.reportid";
+        self::appendFilters($sql, $params);
+        $sql .= " GROUP BY device";
+        $stmnt = DB::$connection->prepare($sql);
+        $stmnt->execute($params);
+
+        $properties = [];
+        while ($row = $stmnt->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT)) {
+            foreach ($row as $key => $value) {
+                if (strcasecmp($key, 'device') != 0) {
+                    if ($value !== null) {
+                        $properties[$key] += $value;
+                    } else {
+                        $properties[$key] = null;
+                    }
+                }
+            }
+        }
+
+        foreach ($properties as $property => &$coverage) {
+            if ($coverage !== null) {
+                $coverage = round($coverage / $deviceCount * 100, 1);
+            }
+        }
+
+        return $properties;
     }
 
     /** Global memory type listings */
