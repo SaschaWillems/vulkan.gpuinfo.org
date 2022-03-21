99870bfe03a4a2e98f6bec7df675d6284d3bf286
Sascha Willems <webmaster@saschawillems.de>
2022-03-21 07:36:44 +0100

    Cleanup

--- listsurfaceusageflags.php
+++ listsurfaceusageflags.php
@@ -59,14 +59,6 @@ PageGenerator::header("Surface usage flags");
 				DB::connect();
 				try {
 					$surfaceusageflags = SqlRepository::listSurfaceUsageFlags($surface_usage_flags);
-					// $os_filter = null;
-					// $params = [];
-					// if ($platform !== 'all') {
-					// 	$params['ostype'] = ostype($platform);
-					// 	$os_filter = 'AND r.ostype = :ostype';
-					// }					
-					// Can't use getDeviceCount here, as some devices that should have no rows in devicesurfacecapabilities (probably custom builds)
-					// $deviceCount = DB::getCount("SELECT count(distinct(r.displayname)) from reports r join devicesurfacecapabilities d on d.reportid = r.id where r.version >= '1.2' $os_filter", $params);
 					foreach ($surfaceusageflags as $surfaceusageflag) {
 						$coverageLink = "listdevicescoverage.php?surfaceusageflag=".$surfaceusageflag['name']."&platform=$platform";
 						$coverage = $surfaceusageflag['coverage'];
@@ -78,27 +70,6 @@ PageGenerator::header("Surface usage flags");
 							echo "</tr>";
 						}
 					}
-					// foreach ($surface_usage_flags as $enum => $flag_name) {
-					// 	$sql = "SELECT
-					// 		count(distinct(r.displayname)) as coverage
-					// 		from devicesurfacecapabilities dsf
-					// 		join reports r on r.id = dsf.reportid
-					// 		where supportedUsageFlags & $enum = $enum
-					// 		$os_filter";
-					// 	$stmnt = DB::$connection->prepare($sql);
-					// 	$stmnt->execute($params);
-					// 	$result = $stmnt->fetch(PDO::FETCH_ASSOC);
-					// 	$coverageLink = "listdevicescoverage.php?surfaceusageflag=$flag_name&platform=$platform";
-					// 	$coverage = $result['coverage'] / $deviceCount * 100.0;
-					// 	if ($coverage > 0) {
-					// 		echo "<tr>";
-					// 		echo "<td class='value'>$flag_name</td>";
-					// 		echo "<td class='text-center'><a class='supported' href='$coverageLink'>" . round($coverage, 1) . "<span style='font-size:10px;'>%</span></a></td>";
-					// 		echo "<td class='text-center'><a class='na' href='$coverageLink&option=not'>" . round(100 - $coverage, 1) . "<span style='font-size:10px;'>%</span></a></td>";
-					// 		echo "</tr>";
-					// 	}
-					// }
-
 				} catch (PDOException $e) {
 					echo "<b>Error while fetcthing data!</b><br>";
 				}
