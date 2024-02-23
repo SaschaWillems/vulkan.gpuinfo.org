b50b3a6443eb6f6635332f8a721d87603dea40f9
Sascha Willems <webmaster@saschawillems.de>
2024-02-23 17:58:41 +0100

    Fix table name

--- api/v4/updatereport.php
+++ api/v4/updatereport.php
@@ -125,7 +125,7 @@ function update_extended_data($json, $reportid, &$update_log)
                     $stmnt_insert = DB::$connection->prepare("INSERT INTO devicefeatures2 (reportid, name, extension, supported) VALUES (:reportid, :name, :extension, :supported)");
                     $stmnt_insert->execute($params);
                     // Mark extension to have additional features
-                    $stmnt_mark = DB::$connection->prepare("UPDATE extension set hasfeatures = 1 where hasproperties is null and name = :extension");
+                    $stmnt_mark = DB::$connection->prepare("UPDATE extensions set hasfeatures = 1 where hasproperties is null and name = :extension");
                     $stmnt_mark->execute(['extension' => $feature['extension']]);		                    
                 }
             }
@@ -159,7 +159,7 @@ function update_extended_data($json, $reportid, &$update_log)
                     $stmnt_insert = DB::$connection->prepare("INSERT INTO deviceproperties2 (reportid, name, extension, value) VALUES (:reportid, :name, :extension, :value)");
                     $stmnt_insert->execute($params);
                     // Mark extension to have additional properties
-                    $stmnt_mark = DB::$connection->prepare("UPDATE extension set hasproperties = 1 where hasproperties is null and name = :extension");
+                    $stmnt_mark = DB::$connection->prepare("UPDATE extensions set hasproperties = 1 where hasproperties is null and name = :extension");
                     $stmnt_mark->execute(['extension' => $feature['extension']]);			                    
                 }
             }
