5d211fecdebcdf50a53822841fba99c5d76d484e
Sascha Willems <webmaster@saschawillems.de>
2026-03-09 20:14:14 +0100

    Cronjob for extension stats

--- cronjobs/updateextensionlistings.php
+++ cronjobs/updateextensionlistings.php
@@ -33,45 +33,59 @@ DB::connect();
 
 $start = microtime(true);
 
-// @todo: mark rows to be deleted (toggle new column active and delete afterwards)
-$stmnt = DB::$connection->prepare("TRUNCATE extension_stats");
-$stmnt->execute();
+// todo: rows for age (all, recent (1y))
 
-$year = NULL;
+$age = NULL;
 
 try {
+    DB::$connection->beginTransaction();
+
+    // Update stats
     $ostypes = [null, 0, 1, 2, 3, 4];
     $apiversions = ['1.0', '1.1', '1.2', '1.3', '1.4'];
     foreach ($ostypes as $ostype) {
         foreach ($apiversions as $apiversion) {
             echo $apiversion."</br>";
-            DB::$connection->beginTransaction();
             $whereClause = "where left(apiversion, 3) >= '$apiversion'";
+            $dateColumn = 'date';
             if (!is_null($ostype)) {
                 $whereClause .= " and ostype = $ostype";
+                $dateColumn = 'date'.strtolower(platformname($ostype));
             }
             $whereClause .= " and r.layered = 0";
-            $sql = "INSERT into extension_stats (name, ostype, apiversion, age, coverage)
-                    SELECT e.name, :ostype, :apiversion, :year, count(distinct(r.displayname)) as coverage
+            $sql = "INSERT into extension_stats (name, ostype, apiversion, age, firstseen, hasfeatures, hasproperties, coverage, state)
+                    SELECT e.name, :ostype, :apiversion, :age,  min(date(e.$dateColumn)), e.hasfeatures, e.hasproperties, count(distinct(r.displayname)) as coverage, 1
                     from deviceextensions de join extensions e on e.id = de.extensionid join reports r on r.id = de.reportid
                     $whereClause
                     group by e.name";
             $params = [
                 "ostype" => $ostype,
-                "year" => $year,
+                "age" => $age,
                 "apiversion" => $apiversion
             ];
             $stmnt = DB::$connection->prepare($sql);
             $stmnt->execute($params);
-            DB::$connection->commit();
-        }
+        }   
     }
+
+    // Delete old rows
+    echo "Deleting old rows<br/>";
+    $stmnt = DB::$connection->prepare("DELETE FROM extension_stats where state = 0");
+    $stmnt->execute();        
+
+    // Mark new rows as active
+    $stmnt = DB::$connection->prepare("UPDATE extension_stats set state = 0 where state = 1");
+    $stmnt->execute();
+
+    DB::$connection->commit();         
 } catch (PDOException $e) {
     echo $e->getMessage();
+    exit();
 }
 
 $delta = (microtime(true) - $start) * 1000;
 echo "Took $delta ms".PHP_EOL;
+echo "success";
 
 DB::disconnect();
 
