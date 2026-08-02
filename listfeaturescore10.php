86dc7697661fe783fc22c4eee88240835df53578
Sascha Willems <webmaster@saschawillems.de>
2026-08-02 17:29:09 +0200

    Anchor view to the left

--- listfeaturescore10.php
+++ listfeaturescore10.php
@@ -4,7 +4,7 @@
  *
  * Vulkan hardware capability database server implementation
  *	
- * Copyright (C) 2016-2024 Sascha Willems (www.saschawillems.de)
+ * Copyright (C) 2016-2026 Sascha Willems (www.saschawillems.de)
  *	
  * This code is free software, you can redistribute it and/or
  * modify it under the terms of the GNU Affero General Public
@@ -30,67 +30,63 @@ PageGenerator::header("Core 1.0 features");
 $platform = PageGenerator::getDefaultOSSelection();
 PageGenerator::pageCaption("Core 1.0 device feature coverage");
 PageGenerator::globalFilterText();
+PageGenerator::platformNavigation('listfeaturescore10.php', $platform, true);
 ?>
 
-<center>
-	<?php PageGenerator::platformNavigation('listfeaturescore10.php', $platform, true); ?>
-
-	<div class='tablediv' style='width:auto; display: inline-block;'>
-		<table id="features" class="table table-striped table-bordered table-hover responsive with-platform-selection">
-			<thead>
-				<tr>
-					<th></th>
-					<th colspan=3 style="text-align: center;">Device coverage</th>
-				</tr>
-				<th>Feature</th>
-				<th style="text-align: center;"><img src='images/icons/check.png' width=16px></th>
-				<th style="text-align: center;"><img src='images/icons/missing.png' width=16px></th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php
-				DB::connect();
-				try {
-					$features = SqlRepository::listCoreFeatures(SqlRepository::VK_API_VERSION_1_0);
-					foreach ($features as $feature => $coverage) {
-						$coverageLink = "listdevicescoverage.php?feature=$feature&platform=$platform";
-						echo "<tr>";
-						echo "<td>$feature</td>";
-						echo "<td class='text-center'><a class='supported' href=\"$coverageLink\">$coverage<span style='font-size:10px;'>%</span></a></td>";
-						echo "<td class='text-center'><a class='na' href=\"$coverageLink&option=not\">".round(100 - $coverage, 2)."<span style='font-size:10px;'>%</span></a></td>";
-						echo "</tr>";
-					}
-				} catch (PDOException $e) {
-					echo "<b>Error while fetching data!</b><br>";
+<div class='tablediv' style='width:auto; display: inline-block;'>
+	<table id="features" class="table table-striped table-bordered table-hover responsive with-platform-selection">
+		<thead>
+			<tr>
+				<th></th>
+				<th colspan=3 style="text-align: center;">Device coverage</th>
+			</tr>
+			<th>Feature</th>
+			<th style="text-align: center;"><img src='images/icons/check.png' width=16px></th>
+			<th style="text-align: center;"><img src='images/icons/missing.png' width=16px></th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php
+			DB::connect();
+			try {
+				$features = SqlRepository::listCoreFeatures(SqlRepository::VK_API_VERSION_1_0);
+				foreach ($features as $feature => $coverage) {
+					$coverageLink = "listdevicescoverage.php?feature=$feature&platform=$platform";
+					echo "<tr>";
+					echo "<td>$feature</td>";
+					echo "<td class='text-center'><a class='supported' href=\"$coverageLink\">$coverage<span style='font-size:10px;'>%</span></a></td>";
+					echo "<td class='text-center'><a class='na' href=\"$coverageLink&option=not\">".round(100 - $coverage, 2)."<span style='font-size:10px;'>%</span></a></td>";
+					echo "</tr>";
 				}
-				DB::disconnect();
-				?>
-			</tbody>
-		</table>
-	</div>
+			} catch (PDOException $e) {
+				echo "<b>Error while fetching data!</b><br>";
+			}
+			DB::disconnect();
+			?>
+		</tbody>
+	</table>
+</div>
 
-	<script>
-		$(document).ready(function() {
-			var table = $('#features').DataTable({
-				"pageLength": -1,
-				"paging": false,
-				"stateSave": false,
-				"searchHighlight": true,
-				"dom": 'f',
-				"bInfo": false,
-				"order": [
-					[0, "asc"]
-				],
-				"columnDefs": [{
-					"targets": [1, 2]
-				}]
-			});
+<script>
+	$(document).ready(function() {
+		var table = $('#features').DataTable({
+			"pageLength": -1,
+			"paging": false,
+			"stateSave": false,
+			"searchHighlight": true,
+			"dom": 'f',
+			"bInfo": false,
+			"order": [
+				[0, "asc"]
+			],
+			"columnDefs": [{
+				"targets": [1, 2]
+			}]
 		});
-	</script>
+	});
+</script>
 
-	<?php PageGenerator::footer(); ?>
+<?php PageGenerator::footer(); ?>
 
-</center>
 </body>
-
 </html>
