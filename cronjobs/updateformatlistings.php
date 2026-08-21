8b3baddee45615de5ee528714651bfb087009f21
Sascha Willems <webmaster@saschawillems.de>
2026-08-21 18:40:13 +0200

    Use feature 2 flags in format listing cronjobs

--- cronjobs/updateformatlistings.php
+++ cronjobs/updateformatlistings.php
@@ -59,17 +59,17 @@ try {
             case 'lineartiling':
                 $column = 'lineartilingfeatures';
                 $parameter_name = 'lineartilingformat';
-                $format_flags = $device_format_flags_tiling;
+                $format_flags = FormatFeatureFlags2::TilingFlags;
                 break;
             case 'optimaltiling':
                 $column = 'optimaltilingfeatures';
                 $parameter_name = 'optimaltilingformat';
-                $format_flags = $device_format_flags_tiling;
+                $format_flags = FormatFeatureFlags2::TilingFlags;
                 break;
             case 'buffer':
                 $column = 'bufferfeatures';
                 $parameter_name = 'bufferformat';
-                $format_flags = $device_format_flags_buffer;
+                $format_flags = FormatFeatureFlags2::BufferFlags;
                 break;
         }
 
