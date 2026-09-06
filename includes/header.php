f02c0fd85214299c15fa783c7e182f66b7513c4d
Sascha Willems <webmaster@saschawillems.de>
2026-09-06 17:59:38 +0200

    Add matomo script

--- includes/header.php
+++ includes/header.php
@@ -62,6 +62,21 @@ session_start();
 			$('body').css('padding-top', parseInt($('#main-navbar').css("height")));
 		});
 	</script>
+	<!-- Matomo -->
+	<script>
+	var _paq = window._paq = window._paq || [];
+	/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+	_paq.push(['trackPageView']);
+	_paq.push(['enableLinkTracking']);
+	(function() {
+		var u="https://gpuinfo.org/matomo/";
+		_paq.push(['setTrackerUrl', u+'matomo.php']);
+		_paq.push(['setSiteId', '1']);
+		var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+		g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+	})();
+	</script>
+	<!-- End Matomo Code -->
 </head>
 <body>
 	<nav class="navbar navbar-default navbar-fixed-top" id="main-navbar">
